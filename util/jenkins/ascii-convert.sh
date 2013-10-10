9d807a91e0aa47f5ffc7395dda646fb1da63cd36
John Jarvis <john@jarv.org>
2013-10-10 16:27:19 -0400

    adding ascii-convert.sh

---
+++ util/jenkins/ascii-convert.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+function ascii_convert {
+  echo $1 | iconv -f utf8 -t ascii//TRANSLIT//IGNORE
+}
+
+# remove non-ascii chars from build user vars
+BUILD_USER_LAST_NAME=$(ascii_convert $BUILD_USER_LAST_NAME)
+BUILD_USER_FIRST_NAME=$(ascii_convert $BUILD_USER_FIRST_NAME)
+BUILD_USER_ID=$(ascii_convert $BUILD_USER_ID)
+BUILD_USER=$(ascii_convert $BUILD_USER)
+
+
