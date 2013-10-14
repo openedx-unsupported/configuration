3706f75b037079cd7c3e1a0ae15aa31bc26d5608
John Jarvis <john@jarv.org>
2013-10-14 14:52:10 -0400

    flock in repos-from-orgs to prevent multiple instances from running

--- playbooks/roles/gh_mirror/files/repos_from_orgs.py
+++ playbooks/roles/gh_mirror/files/repos_from_orgs.py
@@ -13,6 +13,16 @@ import logging
 from os.path import dirname, abspath, join
 from argparse import ArgumentParser
 
+def check_running(run_type=''):
+
+    pid_file = '{}-{}.pid'.format(
+        os.path.basename(__file__),run_type)
+    fp = open(pid_file, 'w')
+    try:
+        fcntl.lockf(fp, fcntl.LOCK_EX | fcntl.LOCK_NB)
+    except IOError:
+        # another instance is running
+        sys.exit(0)
 
 def run_cmd(cmd):
     logging.debug('running: {}\n'.format(cmd))
@@ -74,15 +84,13 @@ def update_repos():
             run_cmd('cd {} && git update-server-info'.format(repo_path))
 
 if __name__ == '__main__':
-    logging.basicConfig(filename='/var/log/repos-from-orgs.log',
-                        level=logging.DEBUG)
     args = parse_args()
     if args.refresh:
+        check_running('refresh')
         refresh_cache()
     else:
+        check_running()
         if not args.datadir:
             print "Please specificy a repository directory"
             sys.exit(1)
         if not os.path.exists('/var/tmp/repos.json'):
-            refresh_cache()
-        update_repos()
