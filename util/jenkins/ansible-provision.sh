768d4d89707fcb829249ea71f1d89a2718416cbe
John Jarvis <john@jarv.org>
2013-10-16 16:46:39 -0400

    renaming PYPI_MIRROR to PYPI_MIRROR_URL

--- util/jenkins/ansible-provision.sh
+++ util/jenkins/ansible-provision.sh
@@ -79,7 +79,7 @@ region: $region
 instance_tags: '{"environment": "$environment", "github_username": "$github_username", "Name": "$name_tag", "source": "jenkins", "owner": "$BUILD_USER"}'
 root_ebs_size: $root_ebs_size
 name_tag: $name_tag
-PYPI_MIRROR: 'https://pypi.edx.org/root/pypi/+simple/'
+PYPI_MIRROR_URL: 'https://pypi.edx.org/root/pypi/+simple/'
 gh_users:
   - user: jarv
     groups:
