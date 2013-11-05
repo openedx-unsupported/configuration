5cd118952ee1f90d70a931166bdb32097f21c16b
John Jarvis <john@jarv.org>
2013-11-05 13:48:00 -0500

    cert->certs

--- util/jenkins/ansible-deploy.sh
+++ util/jenkins/ansible-deploy.sh
@@ -30,6 +30,7 @@ if [[ -z $deploy_host ]]; then
   deploy_host="${github_username}.m.sandbox.edx.org"
 fi
 
+
 cat << EOF > $extra_vars
 ---
 
@@ -56,8 +57,8 @@ XSERVER_GRADER_DIR: "{{ xserver_data_dir }}/data/content-mit-600x~2012_Fall"
 XSERVER_GRADER_SOURCE: "git@github.com:/MITx/6.00x.git"
 XSERVER_LOCAL_GIT_IDENTITY: /var/lib/jenkins/git-identity-edx-pull
 CERTS_LOCAL_GIT_IDENTITY: /var/lib/jenkins/git-identity-edx-pull
-CERTS_AWS_KEY: $(cat /var/lib/jenkins/cert-aws-key)
-CERTS_AWS_ID: $(cat /var/lib/jenkins/cert-aws-id)
+CERTS_AWS_KEY: $(cat /var/lib/jenkins/certs-aws-key)
+CERTS_AWS_ID: $(cat /var/lib/jenkins/certs-aws-id) 
 CERTS_BUCKET: "verify-test.edx.org"
 
 migrate_db: "yes"
