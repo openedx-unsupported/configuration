0e2620adc75379ba7eb51ea22fb605bacd7ea797
John Jarvis <john@jarv.org>
2013-11-05 13:44:01 -0500

    updating deploy script for jenkins

--- util/jenkins/ansible-deploy.sh
+++ util/jenkins/ansible-deploy.sh
@@ -55,6 +55,13 @@ migrate_only: '$run_migration'
 XSERVER_GRADER_DIR: "{{ xserver_data_dir }}/data/content-mit-600x~2012_Fall"
 XSERVER_GRADER_SOURCE: "git@github.com:/MITx/6.00x.git"
 XSERVER_LOCAL_GIT_IDENTITY: /var/lib/jenkins/git-identity-edx-pull
+CERTS_LOCAL_GIT_IDENTITY: /var/lib/jenkins/git-identity-edx-pull
+CERTS_AWS_KEY: $(cat /var/lib/jenkins/cert-aws-key)
+CERTS_AWS_ID: $(cat /var/lib/jenkins/cert-aws-id)
+CERTS_BUCKET: "verify-test.edx.org"
+
+migrate_db: "yes"
+openid_workaround: True
 
 EOF
 
