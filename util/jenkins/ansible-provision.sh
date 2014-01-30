38fc11c994b2df98d04fb8e36757c02c7f9168f3
John Jarvis <john@jarv.org>
2014-01-30 16:59:44 -0500

    updating ami

--- util/jenkins/ansible-provision.sh
+++ util/jenkins/ansible-provision.sh
@@ -75,7 +75,7 @@ fi
 
 if [[ -z $ami ]]; then
   if [[ $server_type == "full_edx_installation" ]]; then
-    ami="ami-09d0ee60"
+    ami="ami-0dd1ef64"
   elif [[ $server_type == "ubuntu_12.04" ]]; then
     ami="ami-d0f89fb9"
   fi
