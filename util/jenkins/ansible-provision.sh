b948f6fad9216a46cf22f6d8f2336dfb05dc0549
John Jarvis <john@jarv.org>
2014-02-18 13:25:27 -0500

    removing override for COMMON_PYPI_MIRROR

--- util/jenkins/ansible-provision.sh
+++ util/jenkins/ansible-provision.sh
@@ -107,7 +107,6 @@ EDXAPP_LMS_NGINX_PORT: 80
 EDXAPP_LMS_PREVIEW_NGINX_PORT: 80
 EDXAPP_CMS_NGINX_PORT: 80
 EDXAPP_SITE_NAME: ${deploy_host}
-COMMON_PYPI_MIRROR_URL: 'https://pypi.edx.org/root/pypi/+simple/'
 XSERVER_GRADER_DIR: "/edx/var/xserver/data/content-mit-600x~2012_Fall"
 XSERVER_GRADER_SOURCE: "git@github.com:/MITx/6.00x.git"
 XSERVER_LOCAL_GIT_IDENTITY: /var/lib/jenkins/git-identity-edx-pull
