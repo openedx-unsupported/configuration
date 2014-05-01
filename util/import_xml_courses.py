8f772890e01af12c0511574c0f90ddf5073278e7
Feanil Patel <feanil@edx.org>
2014-05-01 11:19:22 -0400

    Convert import script to ansible tasks.
    Require versions for the course repo checkouts so we can match the versions in
    production.
    
    Update import script to spit out yaml configuration and XML mappings for mixed
    module store.

--- util/import_xml_courses.py
+++ util/import_xml_courses.py
@@ -10,116 +10,53 @@
 #    course:
 #    run:
 #    disposition:
+#    version:
 #}
 
 import argparse
-import logging
-import subprocess
-from os.path import basename, exists, join
-import os
-import contextlib
-from getpass import getuser
-
-logging.basicConfig(level=logging.DEBUG)
-
-@contextlib.contextmanager
-def chdir(dirname=None):
-  curdir = os.getcwd()
-  try:
-    if dirname is not None:
-      os.chdir(dirname)
-    yield
-  finally:
-    os.chdir(curdir)
+from os.path import basename
+import yaml
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description="Import XML courses from git repos.")
     parser.add_argument("-c", "--courses-csv", required=True,
         help="A CSV of xml courses to import.")
-    parser.add_argument("-d", "--var-dir", default="/edx/var/edxapp",
-        help="The location of the edxapp var directory that hold the data and courstatic dirs.")
     args = parser.parse_args()
 
     courses = open(args.courses_csv, 'r')
-    data_dir = join(args.var_dir, 'data')
-    static_dir = join(args.var_dir, 'course_static')
 
-
-    cwd = getcwd()
-    platform_dir = "/edx/app/edxapp/edx-platform"
-
+    all_course_data = []
+    all_xml_mappings = {}
     for line in courses:
         cols = line.strip().split(',')
         slug = cols[0]
         author_format = cols[1]
         disposition = cols[2]
         repo_url = cols[4]
+        version = cols[5]
 
         if author_format.lower() != 'xml' \
           or disposition.lower() == "don't import":
             continue
 
         # Checkout w/tilde
-        logging.debug("{}: {}".format(slug, repo_url))
         org, course, run = slug.split("/")
         repo_name = "{}~{}".format(basename(repo_url).rstrip('.git'), run)
 
-        # Clone course into data dir.
-        repo_location = join(args.data_dir, repo_name)
-        if not exists(repo_location):
-            cmd = "git clone {} {}".format(repo_url, repo_location)
-            subprocess.check_call(cmd,shell=True)
-            chown_cmd = "chown -R www-data:edxapp {}".format(repo_location)
-            subprocess.check_call(chown_cmd, shell=True)
-
-        # Update course.xml
-        course_xml_path = join(repo_location, "course.xml")
-        xml_content = '<course org="{}" course="{}" url_name="{}"/>'
-        f = open(course_xml_path, 'w')
-        f.write(xml_content.format(org, course, run))
-        f.close()
-
+        course_info = {
+            "repo_url": repo_url,
+            "repo_name": repo_name,
+            "org": org,
+            "course": course,
+            "run": run,
+            "disposition": disposition.lower(),
+            "version": version,
+        }
+        all_course_data.append(course_info)
 
         if disposition.lower() == "on disk":
-            with chdir(args.static_dir):
-                # Link static files to course_static dir
-                static_location = join(repo_location, 'static')
-                cmd = "ln -sf {} {}".format(static_location, repo_name)
-                logging.debug("Running cmd: {}".format(cmd))
-                subprocess.check_call(cmd, shell=True)
-        elif disposition.lower() == 'no static import':
-            # Go to the platform dir.
-            # Need this for dealer to work.
-            with chdir(platform_dir):
-                # Import w/nostatic flag
-                cmd = "sudo -E -u edxapp /edx/bin/python.edxapp /edx/app/edxapp/edx-platform/manage.py cms --settings=aws import --nostatic {} {}".format(args.data_dir, repo_name)
-                logging.debug("Running cmd: {}".format(cmd))
-                subprocess.check_call(cmd, shell=True)
+            all_xml_mappings[slug] = 'xml'
 
-            with chdir(args.static_dir):
-                # Link static files to course_static dir.
-                static_location = join(repo_location, 'static')
-                cmd = "ln -sf {} {}".format(static_location, repo_name)
-                logging.debug("Running cmd: {}".format(cmd))
-                subprocess.check_call(cmd, shell=True)
-
-        elif disposition.lower() == 'import':
-            # Go to the platform dir.
-            # Need this for dealer to work.
-            with chdir(platform_dir):
-                # Import
-                cmd = "sudo -E -u edxapp /edx/bin/python.edxapp /edx/app/edxapp/edx-platform/manage.py cms --settings=aws import {} {}".format(args.data_dir, repo_name)
-                logging.debug("Running cmd: {}".format(cmd))
-                subprocess.check_call(cmd, shell=True)
-
-            # Remove from disk.
-            cmd = "sudo rm -rf {}".format(repo_location)
-            logging.debug("Running cmd: {}".format(cmd))
-            subprocess.check_call(cmd, shell=True)
-
-    # Create the tar file of all xml courses
-    cmd = "sudo tar cvzf static_course_content.tar.gz -C {} data course_static".format(args.var_dir)
-    logging.debug("Running cmd: {}".format(cmd))
-    subprocess.check_call(cmd, shell=True)
-
+    edxapp_xml_courses = { "EDXAPP_XML_COURSES": all_course_data, "EDXAPP_XML_MAPPINGS": all_xml_mappings }
+    print yaml.safe_dump(edxapp_xml_courses, default_flow_style=False)
