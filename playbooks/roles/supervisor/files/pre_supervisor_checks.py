4af0cc41a32c3806202e43535499c0069aa320df
Feanil Patel <feanil@edx.org>
2014-05-19 11:02:00 -0400

    Add support for hipchat notification from pre-supervisor script.

--- playbooks/roles/supervisor/files/pre_supervisor_checks.py
+++ playbooks/roles/supervisor/files/pre_supervisor_checks.py
@@ -4,6 +4,8 @@ import boto
 import boto.utils
 import os
 import subprocess
+import hipchat
+import traceback
 
 
 if __name__ == '__main__':
@@ -14,24 +16,55 @@ if __name__ == '__main__':
     parser.add_argument("-e","--enabled",
         help="The location of the enabled services.")
 
+    hipchat_args = parser.add_argument_group("hipchat",
+            "Args for hipchat notification.")
+    hipchat_args.add_argument("-c","--hipchat-api-key",
+        help="Hipchat token if you want to receive notifications via hipchat.")
+    hipchat_args.add_argument("-r","--hipchat-room",
+        help="Room to send messages to.")
+
     args = parser.parse_args()
 
-    ec2 = boto.connect_ec2()
-    instance_id = boto.utils.get_instance_metadata()['instance-id']
-    reservations = ec2.get_all_instances(instance_ids=[instance_id])
+    hc = None
     report = []
-    for reservation in reservations:
-        for instance in reservation.instances:
-            if instance.id == instance_id:
-                services = instance.tags['services'].split(',')
-                for service in services:
-                    # Link to available service.
-                    available_file = "{}/{}.conf".format(args.available, service)
-                    link_location = "{}/{}.conf".format(args.enabled, service)
-                    if os.path.exists(available_file):
-                        subprocess.call("ln -sf {} {}".format(available_file, link_location), shell=True)
-                        report.append("Linking service: {}".format(service))
-                    else:
-                        report.append("No conf available for service: {}".format(link_location))
+    hc_user = "PreSupervisor"
 
-    print("\n".join(report))
+    if args.hipchat_api_key:
+        hc = hipchat.HipChat(token=args.hipchat_api_key)
+
+    try:
+        ec2 = boto.connect_ec2()
+        instance_id = boto.utils.get_instance_metadata()['instance-id']
+        reservations = ec2.get_all_instances(instance_ids=[instance_id])
+        for reservation in reservations:
+            for instance in reservation.instances:
+                if instance.id == instance_id:
+                    try:
+                        services = instance.tags['services'].split(',')
+                    except KeyError as ke:
+                        msg = "Tag named 'services' not found on this instance({})".format(instance_id)
+                        raise Exception(msg)
+
+                    for service in services:
+                        # Link to available service.
+                        available_file = os.path.join(args.available, "{}.conf".format(service))
+                        link_location = os.path.join(args.enabled, "{}.conf".format(service))
+                        if os.path.exists(available_file):
+                            subprocess.call("ln -sf {} {}".format(available_file, link_location), shell=True)
+                            report.append("Linking service: {}".format(service))
+                        else:
+                            raise Exception("No conf available for service: {}".format(link_location))
+    except Exception as e:
+        msg = "{}: pre_supervisor failed with exception: {}".format(instance_id, traceback.format_exc())
+        print(msg)
+        if hc:
+            hc.message_room(room_id=args.hipchat_room,
+            message_from=hc_user,
+            message=msg)
+    finally:
+        print("\n".join(report))
+        if hc:
+            hc.message_room(room_id=args.hipchat_room,
+                message_from=hc_user,
+                message="{}:\n{}".format(instance_id,"\n".join(report)))
+
