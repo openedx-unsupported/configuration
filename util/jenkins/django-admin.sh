476448c3ccf30f3e8bf548c133994850cac4013b
Han Su Kim <han@edx.org>
2014-08-19 14:56:54 -0400

    Squashing Commits
    New script to add to cert whitelist, modification to generate cert to allow for just one user to be generated
    
    Minor changes to make it more readable
    
    Typos
    
    Adding jenkins script to run manage.py
    
    Adding jenkins script to change course enrollment
    
    Scripts for edge-vpc

---
+++ util/jenkins/django-admin.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+cd configuration
+pip install -r requirements.txt
+env
+
+
+stageip=$(python playbooks/ec2.py | jq -r '."tag_Name_stage-edx-worker"[0] | strings')
+prodip=$(python playbooks/ec2.py | jq -r '."tag_Name_prod-edx-worker"[0] | strings')
+
+if [ "$cluster" = "prod" ]; then
+  ip=$stageip
+else
+  ip=$prodip
+fi
+
+ssh="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
+manage="cd /edx/app/edxapp/edx-platform && sudo -u www-data /edx/bin/python.edxapp ./manage.py"
+
+if [ "$service_variant" != "UNSET" ]; then
+  manage="$manage $service_variant"
+fi
+
+if [ "$help" = "true" ]; then
+  manage="$manage help"
+fi
+
+$ssh ubuntu@"$ip" "$manage $command $options --settings aws"
--- util/jenkins/issue-certificates-edge.sh
+++ util/jenkins/issue-certificates-edge.sh
@@ -4,7 +4,7 @@ cd configuration
 pip install -r requirements.txt
 env
 
-ip=$(python playbooks/ec2.py | jq -r '."tag_Name_prod-edx-worker"[0] | strings')
+ip=$(python playbooks/ec2.py | jq -r '."tag_Name_prod-edge-worker"[0] | strings')
 ssh="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
 manage="cd /edx/app/edxapp/edx-platform && sudo -u www-data /edx/bin/python.edxapp ./manage.py"
 
