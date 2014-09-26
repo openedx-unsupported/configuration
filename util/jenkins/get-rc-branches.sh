e4230a3a808d7d49347cb2c927fe5777461431d6
Han Su Kim <han@edx.org>
2014-09-26 11:28:07 -0400

    Adding logic to list by tag as well as branch

--- util/jenkins/get-rc-branches.sh
+++ util/jenkins/get-rc-branches.sh
@@ -63,8 +63,11 @@ fi
 $noop cd "/var/tmp/$repo_basename"
 if [[ -z $noop ]]; then
     for branch in $(git branch -a | sort -r | tr -d ' ' | grep -E "$filter" ); do
-      echo "origin/${branch}"
+        echo "origin/${branch}"
     done
-else
-    echo "Would have checked for branches using filter $filter"
+    for tag in $(git tag -l | sort -r | tr -d ' ' | grep -E "$filter"); do
+        echo "$tag"
+    done
+    else
+        echo "Would have checked for branches or tags using filter $filter"
 fi
