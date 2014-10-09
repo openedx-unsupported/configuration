9e687d3739247ed263d730f0342a9768af090459
Han Su Kim <han@edx.org>
2014-10-09 13:50:41 -0400

    Seperating out Xqueue and xqueue_consumer restarts

--- util/jenkins/restart-xqueue_consumer.sh
+++ util/jenkins/restart-xqueue_consumer.sh
@@ -4,6 +4,6 @@ cd configuration
 pip install -r requirements.txt
 env
 
-command="/edx/bin/supervisorctl restart xqueue"
+command="/edx/bin/supervisorctl restart xqueue_consumer"
 
 ansible tag_Name_${environment}-${deployment}-commoncluster -i playbooks/ec2.py -u ubuntu -s -a "$command"
