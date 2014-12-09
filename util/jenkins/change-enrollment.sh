bd2b9f8c3b62e4f66c768e5b2126d2b75108046c
Edward Zarecor <ed@edx.org>
2014-12-08 21:04:49 -0500

    fix var name.

--- util/jenkins/change-enrollment.sh
+++ util/jenkins/change-enrollment.sh
@@ -4,10 +4,10 @@ env
 
 ansible="ansible first_in_tag_Name_${environment}-${deployment}-worker -i playbooks/ec2.py -u ubuntu -s -U www-data -a"
 manage="/edx/bin/python.edxapp /edx/bin/manage.edxapp lms change_enrollment --settings=aws"
-noop=""
+noop_switch=""
 
 if [ "$noop" = true ]; then
-  noop="--noop"  
+  noop_switch="--noop"  
 fi
 
-$ansible "$manage $noop --course $course --user $name --to $to --from $from"
+$ansible "$manage $noop_switch --course $course --user $name --to $to --from $from"
---
+++ vagrant/release/devstack/nfs.sh
@@ -0,0 +1,4 @@
+systemctl enable rpcbind.service 
+systemctl enable nfs-server.service 
+systemctl enable nfs-lock.service 
+systemctl enable nfs-idmap.service 
