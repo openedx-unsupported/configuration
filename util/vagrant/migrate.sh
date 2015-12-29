0675383798ffc72d80a14bcdc08b897cd0a45d91
Ned Batchelder <ned@edx.org>
2015-12-29 10:01:29 -0500

    Update migrate.sh for Dogwood upgrades

--- util/vagrant/migrate.sh
+++ util/vagrant/migrate.sh
@@ -6,6 +6,14 @@ TARGET="named-release/cypress"
 INTERACTIVE=true
 OPENEDX_ROOT="/edx"
 
+# Will exit the script if a command fails.
+bail_if_fail () {
+  exitcode=$?
+  if [ $exitcode != 0 ]; then
+    exit $exitcode;
+  fi
+}
+
 read -d '' HELP_TEXT <<- EOM
 Attempts to migrate your Open edX installation to a different release.
 
@@ -44,6 +52,16 @@ while getopts "hc:t:y" opt; do
       ;;
   esac
 done
+
+if [[ `whoami` != "vagrant" ]]; then
+  echo "Run this from the vagrant account in your Open edX machine."
+  exit 1
+fi
+
+if [[ ! -d /edx/app/edxapp ]]; then
+  echo "Run this from the vagrant account in your Open edX machine."
+  exit 1
+fi
 
 if [[ $TARGET == *birch* && $INTERACTIVE == true ]] ; then
   cat <<"EOM"
@@ -95,6 +113,7 @@ EOM
 fi
 
 if [[ $TARGET == *cypress* ]] ; then
+  # Needed if transitioning to Cypress.
   echo "Killing all celery worker processes."
   sudo ${OPENEDX_ROOT}/bin/supervisorctl stop edxapp_worker:* &
   sleep 3
@@ -113,30 +132,84 @@ if [ -f /edx/app/edx_ansible/server-vars.yml ]; then
   SERVER_VARS="--extra-vars=\"@${OPENEDX_ROOT}/app/edx_ansible/server-vars.yml\""
 fi
 
+make_config_venv () {
+  virtualenv venv
+  source venv/bin/activate
+  pip install -r configuration/pre-requirements.txt
+  pip install -r configuration/requirements.txt
+}
+
 TEMPDIR=`mktemp -d`
+echo "Working in $TEMPDIR"
 chmod 777 $TEMPDIR
 cd $TEMPDIR
-git clone https://github.com/edx/configuration.git --depth=1 --single-branch --branch=$TARGET
-virtualenv venv
-source venv/bin/activate
-pip install -r configuration/requirements.txt
+# Set the CONFIGURATION_TARGET environment variable to use a different branch
+# in the configuration repo, defaults to $TARGET.
+git clone https://github.com/edx/configuration.git \
+  --depth=1 --single-branch --branch=${CONFIGURATION_TARGET-$TARGET}
+make_config_venv
+
+if [[ $TARGET == *dogwood* ]] ; then
+  # We are upgrading from 2.7.3 to 2.7.10, so remake the venvs.
+  sudo rm -rf /edx/app/*/v*envs/*
+
+  if [[ $CONFIGURATION == devstack ]] ; then
+    DEVSTACK_VARS="--extra-vars=devstack=true"
+  fi
+
+  echo "Upgrading edx-platform to the end of Django 1.4"
+  cd configuration/playbooks/vagrant
+  sudo ansible-playbook \
+    --inventory-file=localhost, \
+    --connection=local \
+    $SERVER_VARS \
+    $DEVSTACK_VARS \
+    --extra-vars="edx_platform_version=release-2015-11-09 migrate_db=yes" \
+    --skip-tags="edxapp-sandbox" \
+    vagrant-edxapp-delta.yml
+  bail_if_fail
+  cd ../../..
+
+  # That playbook updated Python from 2.7.3 to 2.7.10, so remake our own venv.
+  rm -rf venv
+  make_config_venv
+
+  # Need to get rid of South from edx-platform, or things won't work.
+  sudo -u edxapp /edx/app/edxapp/venvs/edxapp/bin/pip uninstall -y South
+  bail_if_fail
+
+  echo "Upgrading edx-platform to the beginning of Django 1.8"
+  cd configuration/playbooks/vagrant
+  sudo ansible-playbook \
+    --inventory-file=localhost, \
+    --connection=local \
+    $SERVER_VARS \
+    $DEVSTACK_VARS \
+    --extra-vars="edx_platform_version=release-2015-11-17 migrate_db=no" \
+    --skip-tags="edxapp-sandbox" \
+    vagrant-edxapp-delta.yml
+  bail_if_fail
+  cd ../../..
+
+  echo "Running the first Django 1.8 faked migrations"
+  sudo -u edxapp /edx/app/edxapp/venvs/edxapp/bin/python /edx/app/edxapp/edx-platform/manage.py lms --settings=aws migrate --fake-initial
+  sudo -u edxapp /edx/app/edxapp/venvs/edxapp/bin/python /edx/app/edxapp/edx-platform/manage.py cms --settings=aws migrate --fake-initial
+fi
+
+cd configuration/playbooks
 echo "edx_platform_version: $TARGET" >> vars.yml
 echo "ora2_version: $TARGET" >> vars.yml
 echo "certs_version: $TARGET" >> vars.yml
 echo "forum_version: $TARGET" >> vars.yml
 echo "xqueue_version: $TARGET" >> vars.yml
-cd configuration/playbooks
 sudo ansible-playbook \
     --inventory-file=localhost, \
     --connection=local \
     --extra-vars=\"@../../vars.yml\" \
     $SERVER_VARS \
     vagrant-$CONFIGURATION.yml
-# if this failed, bail out early
-exitcode=$?
-if [ $exitcode != 0 ]; then
-  exit $exitcode;
-fi
+bail_if_fail
+
 cd /
 sudo rm -rf $TEMPDIR
 echo "Migration complete. Please reboot your machine."
