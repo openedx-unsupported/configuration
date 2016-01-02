f8b169a400fd2b728debda0a562d871d22105f07
edxhneu <edx@hneu.net>
2016-01-02 20:46:17 +0200

    Fix dependence of installtion "pip install MySQL-python"

--- util/install/sandbox.sh
+++ util/install/sandbox.sh
@@ -31,7 +31,7 @@ sudo apt-get upgrade -y
 ##
 ## Install system pre-requisites
 ##
-sudo apt-get install -y build-essential software-properties-common python-software-properties curl git-core libxml2-dev libxslt1-dev python-pip python-apt python-dev libxmlsec1-dev libfreetype6-dev swig gcc-4.8 g++-4.8
+sudo apt-get install -y build-essential software-properties-common python-software-properties curl git-core libxml2-dev libxslt1-dev python-pip python-apt python-dev libxmlsec1-dev libfreetype6-dev swig libmysqlclient-dev gcc-4.8 g++-4.8
 sudo pip install --upgrade pip
 sudo -H pip install --upgrade virtualenv
 
