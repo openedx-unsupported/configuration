51b704ade95a6012fbd9554a118168ceec82ac77
Fred Smith <derf@edx.org>
2016-01-12 12:07:39 -0500

    updated sandbox ami

--- util/jenkins/ansible-provision.sh
+++ util/jenkins/ansible-provision.sh
@@ -99,7 +99,7 @@ fi
 
 if [[ -z $ami ]]; then
   if [[ $server_type == "full_edx_installation" ]]; then
-    ami="ami-52c18038"
+    ami="ami-9d80dbf7"
   elif [[ $server_type == "ubuntu_12.04" || $server_type == "full_edx_installation_from_scratch" ]]; then
     ami="ami-c15bebaa"
   elif [[ $server_type == "ubuntu_14.04(experimental)" ]]; then
