cd6dba94c75f24c52e91fb4d8932c9cfc30ceb2f
Feanil Patel <feanil@edx.org>
2016-04-25 17:35:06 -0400

    Be able to override the git identity.

--- util/jenkins/build-ami.sh
+++ util/jenkins/build-ami.sh
@@ -115,6 +115,11 @@ if [[ ! -z "$region" ]]; then
   region_params="--region $region"
 fi
 
+identity_params="--identity /edx/var/jenkins/.ssh/id_rsa"
+if [[ ! -z "$identity_path" ]]; then
+  identity_params="--identity $identity_path"
+fi
+
 cd configuration
 pip install -r requirements.txt
 
@@ -122,4 +127,4 @@ cd util/vpc-tools/
 
 echo "$vars" > /var/tmp/$BUILD_ID-extra-vars.yml
 cat /var/tmp/$BUILD_ID-extra-vars.yml
-python -u abbey.py -p $play -t m3.large -d $deployment -e $environment -i /edx/var/jenkins/.ssh/id_rsa $base_params $blessed_params $playbookdir_params --vars /var/tmp/$BUILD_ID-extra-vars.yml -c $BUILD_NUMBER --configuration-version $configuration --configuration-secure-version $configuration_secure -k $jenkins_admin_ec2_key --configuration-secure-repo $jenkins_admin_configuration_secure_repo $configurationprivate_params $hipchat_params $cleanup_params $notification_params $datadog_params $region_params
+python -u abbey.py -p $play -t m3.large -d $deployment -e $environment $base_params $blessed_params $playbookdir_params --vars /var/tmp/$BUILD_ID-extra-vars.yml -c $BUILD_NUMBER --configuration-version $configuration --configuration-secure-version $configuration_secure -k $jenkins_admin_ec2_key --configuration-secure-repo $jenkins_admin_configuration_secure_repo $configurationprivate_params $hipchat_params $cleanup_params $notification_params $datadog_params $region_params $identity_params
