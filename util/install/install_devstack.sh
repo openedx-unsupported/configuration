68c34ad78515efe4abec4f85547264c1bd43c1b1
Zachary Robbins <zrrobbins@wpi.edu>
2016-07-18 15:56:54 -0400

    Install script will work with master and most-recent named-release (dogwood) versions only. Will not work with weekly releases, or previous named-releases.

--- util/install/install_devstack.sh
+++ util/install/install_devstack.sh
@@ -2,8 +2,6 @@
 
 # Stop if any command fails
 set -e
-# Echo all commands
-set -x
 
 function usage
 {
@@ -16,16 +14,20 @@ function usage
     Installs the Open edX developer stack. More information on installing devstack 
     can be found here: https://openedx.atlassian.net/wiki/display/OpenOPS/Running+Devstack
 
+    NOTE: This script assumes you have never installed devstack before. Installing multiple 
+    versions of devstack can often cause conflicts that this script is not prepared to handle. 
+    Reach out on the edX community Slack if you need more help in this topic. 
+
     release
         The release of Open edX you wish to run. Upgrade to the given git ref 'release'.
-        You must specify this. Named released are called "named-release/cypress",
+        You must specify this. Named released are called "named-release/dogwood",
         "named-release/dogwood.2", and so on. We recommend the latest stable named 
         release for general members of the open source community. Named releases can
         be found at: https://openedx.atlassian.net/wiki/display/DOC/Open+edX+Releases.
-        If you plan on modifying the code, we recommend the "release" branch.
+        If you plan on modifying the code, we recommend the "master" branch.
 
     -p
-        Enable use of "preview" from within Studio.
+        Enable use of "preview" from within Studio. 
 
     -v vagrant_mount_base
         Customize the location of the source code that gets cloned during the 
@@ -44,7 +46,7 @@ mkdir -p install_logs
 exec > >(tee install_logs/install-$(date +%Y%m%d-%H%M%S).log) 2>&1
 echo "Logs located in install_logs directory"
 
-# Default OPENEDX_RELEASE
+# OPENEDX_RELEASE
 release=""
 # Enable preview in Studio
 enable_preview=0
@@ -74,25 +76,34 @@ while getopts "r:pv:h" opt; do
     esac
 done
 
+WARN='\033[1;33m' # Yellow
+NC='\033[0m' # No Color
 
-export OPENEDX_RELEASE=$release
-mkdir -p devstack
-cd devstack
-
-# Install devstack
-curl -L https://raw.githubusercontent.com/edx/configuration/master/vagrant/release/devstack/Vagrantfile > Vagrantfile
-vagrant plugin install vagrant-vbguest
-vagrant up --provider virtualbox
-
-# Check if preview mode was chosen
-if [[ $enable_preview -eq 1 ]]; then
-    echo "Enabling use of preview within Studio..."
-    sudo bash -c "echo '192.168.33.10  preview.localhost' >> /etc/hosts"
+if [[ $release != "master" ]]; then
+    echo -e "${WARN}The release you entered is not 'master'. Please be aware that a branch other than master or a release other than the latest named-release could cause errors.${NC}"
 fi
+
 # Check if mount location was changed
 if [[ $vagrant_mount_location != "" ]]; then
     echo "Changing Vagrant provision location to "$vagrant_mount_location"..."
     export VAGRANT_MOUNT_BASE=vagrant_mount_location
 fi
 
+export OPENEDX_RELEASE=$release
+mkdir -p devstack
+cd devstack
+
+# Install devstack
+curl -L https://raw.githubusercontent.com/edx/configuration/${OPENEDX_RELEASE}/vagrant/release/devstack/Vagrantfile > Vagrantfile
+vagrant plugin install vagrant-vbguest
+vagrant up --provider virtualbox
+
+# Check if preview mode was chosen
+if [[ $enable_preview -eq 1 ]]; then
+    echo "Enabling use of preview within Studio..."
+    sudo bash -c "echo '192.168.33.10 preview.localhost' >> /etc/hosts"
+fi
+
+
 echo "Finished installing! You may now login using 'vagrant ssh'"
+echo "Refer to the edX wiki for more information on using devstack."
