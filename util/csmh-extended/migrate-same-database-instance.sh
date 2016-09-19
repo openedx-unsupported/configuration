fd7a8fa225d0a9985cb56f7ae935540cf88801b6
Dmitry Gamanenko <dgamanenko@gamail.com>
2016-09-19 10:11:33 +0000

    update edxapp_csmh migrate script

--- util/csmh-extended/migrate-same-database-instance.sh
+++ util/csmh-extended/migrate-same-database-instance.sh
@@ -1,17 +1,18 @@
+#!/bin/bash
 MINID=0
 MAXID=1003426362
 STEP=10000
 MIGRATE_USER=migrate
-PASSWORD='secret-password'
-HOST='my-database-instance'
+PASSWORD='password'
+HOST='localhost'
  
  
 for ((i=0; i<=$MAXID; i+=$STEP)); do
 echo -n "$i";
-mysql -u $MIGRATE_USER -p$PASSWORD -h $HOST wwc <<EOF
+mysql -u $MIGRATE_USER -p$PASSWORD -h $HOST edxapp <<EOF
 INSERT INTO edxapp_csmh.coursewarehistoryextended_studentmodulehistoryextended (id, version, created, state, grade, max_grade, student_module_id)
   SELECT id, version, created, state, grade, max_grade, student_module_id
-  FROM wwc.courseware_studentmodulehistory
+  FROM edxapp.courseware_studentmodulehistory
   WHERE id BETWEEN $i AND $(($i+$STEP-1));
 EOF
 echo '.';
