685f3250ca0fa754602b6c7ff679aa64ee8d9542
Ned Batchelder <ned@edx.org>
2016-12-01 13:01:15 -0500

    Use the generic vagrant public key

--- vagrant/osbase/xenial/scripts/vagrant.sh
+++ vagrant/osbase/xenial/scripts/vagrant.sh
@@ -2,7 +2,7 @@
 
 mkdir -p /home/vagrant/.ssh
 wget --no-check-certificate \
-    'https://gist.githubusercontent.com/vadviktor/5cb616f164aa2e4f266a/raw/b0c22c8d34d5a3e7a1e596c4e2f55bcd7111951f/vagrant_rsa.pub' \
+    https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub \
     -O /home/vagrant/.ssh/authorized_keys
 chown -R vagrant /home/vagrant/.ssh
 chmod -R go-rwsx /home/vagrant/.ssh
