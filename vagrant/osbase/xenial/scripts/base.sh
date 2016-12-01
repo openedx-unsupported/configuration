9eb786fe2954b85d2fc7920480a38959644f1dcd
Ned Batchelder <ned@edx.org>
2016-12-01 12:58:20 -0500

    Don't need two ways to give vagrant sudo; use recommended swappiness

--- vagrant/osbase/xenial/scripts/base.sh
+++ vagrant/osbase/xenial/scripts/base.sh
@@ -14,9 +14,8 @@ sed -i -e 's/noatime,/noatime,acl,/g' /etc/fstab
 
 echo %vagrant ALL=NOPASSWD:ALL > /etc/sudoers.d/vagrant
 chmod 0440 /etc/sudoers.d/vagrant
-usermod -a -G sudo vagrant
 
 echo "UseDNS no" >> /etc/ssh/sshd_config
 
 echo "fs.inotify.max_user_watches = 524288" >> /etc/sysctl.conf
-echo "vm.swappiness = 1" >> /etc/sysctl.conf
+echo "vm.swappiness = 10" >> /etc/sysctl.conf
