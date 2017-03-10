fa6f2b461ccc5241b2add075c5555cfdc356b218
melvinsoft <melvinsoft@gmail.com>
2017-03-10 16:40:42 -0300

    fix for add machine name on tracking logs for enterprise

--- playbooks/roles/tracking_logs_sync/files/copy_tracking_logs.py
+++ playbooks/roles/tracking_logs_sync/files/copy_tracking_logs.py
@@ -14,7 +14,7 @@ import boto
 import raven
 
 
-def upload_to_gcloud_storage(file_path, bucket):
+def upload_to_gcloud_storage(file_path, bucket, multiple_edxapp_vm):
     """
     Upload a file to the specified Google Cloud Storage bucket.
 
@@ -32,6 +32,10 @@ def upload_to_gcloud_storage(file_path, bucket):
                  '"{}"'.format(file_path, bucket))
 
     file_name = os.path.basename(file_path)
+
+    if multiple_edxapp_vm and multiple_edxapp_vm == "yes":
+        file_name = '%s-%s%s' % (file_name[:-3], socket.gethostname(), file_name[-3:])
+
     gcloud_uri = boto.storage_uri(bucket + '/' + file_name, 'gs')
     gcloud_uri.new_key().set_contents_from_filename(file_path)
     logging.info('Upload successful')
@@ -42,6 +46,7 @@ def _parse_args():
     parser.add_argument('-p', '--provider', help='gs or s3')
     parser.add_argument('-b', '--bucket', help='bucket name')
     parser.add_argument('--sentry-dsn', help='Sentry data source name')
+    parser.add_argument('--multiple-edxapp-vm', help='Multiple edxapp vm')
 
     return parser.parse_args()
 
@@ -52,13 +57,14 @@ def _main():
     bucket = args.bucket or os.environ.get('TRACKING_LOGS_BUCKET')
     provider = args.provider or os.environ.get('TRACKING_LOGS_STORAGE_PROVIDER', 'gs')
     sentry_dsn = args.sentry_dsn or os.environ.get('TRACKING_LOGS_SENTRY_DSN', '')
+    multiple_edxapp_vm = args.multiple_edxapp_vm or os.environ.get('TRACKING_LOGS_MULTIPLE_EDXAPP_VM', False)
 
     sentry = raven.Client(sentry_dsn)
     tracking_logs_dir = "/edx/var/log/tracking/"
 
     for tl_filename in os.listdir(tracking_logs_dir):
         try:
-            upload_to_gcloud_storage(tracking_logs_dir + tl_filename, bucket)
+            upload_to_gcloud_storage(tracking_logs_dir + tl_filename, bucket, multiple_edxapp_vm)
         except:
             logging.exception("The tracking logs copy has failed!")
             sentry.captureException()
