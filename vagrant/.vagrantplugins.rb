3f03533e42656358eab81b598630f7e06fecc685
UmanShahzad <emidanrko564@hotmail.com>
2017-04-30 10:33:00 -0400

    Automate required plugin installation.

---
+++ vagrant/.vagrantplugins.rb
@@ -0,0 +1,33 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+def install_plugins(plugins)
+  not_installed = []
+  plugins.each do |plugin|
+    unless Vagrant.has_plugin?(plugin)
+      not_installed << plugin
+    end
+  end
+
+  if not_installed.any?
+    puts "The following required plugins must be installed:"
+    puts "'#{not_installed.join("', '")}'"
+    print "Install? [y]/n: "
+    unless STDIN.gets.chomp == "n"
+      not_installed.each { |plugin| install_plugin(plugin) }
+    else
+      exit
+    end
+    $? ? continue : raise('Plugin installation failed, see errors above.')
+  end
+end
+
+def install_plugin(plugin)
+  system "vagrant plugin install #{plugin}"
+end
+
+# If plugins successfully installed, restart vagrant to detect changes.
+def continue
+  exec "vagrant #{ARGV[0]}"
+end
+
