9f6844ca5857e6d0de1eb4b28fbc4f059f57f7a2
Kevin Falcone <kevin@edx.org>
2017-06-21 10:19:21 -0400

    New sandbox base image

--- util/jenkins/ansible-provision.sh
+++ util/jenkins/ansible-provision.sh
@@ -127,7 +127,7 @@ fi
 
 if [[ -z $ami ]]; then
   if [[ $server_type == "full_edx_installation" ]]; then
-    ami="ami-dca185ca"
+    ami="ami-c20128d4"
   elif [[ $server_type == "ubuntu_16.04" || $server_type == "full_edx_installation_from_scratch" ]]; then
     ami="ami-20631a36"
   fi
