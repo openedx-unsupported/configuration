2c02b97ff8b09440c2fba6e207c96b26c06f242c
Shirley He <shirleyhe@C02T53Z1FVH4.tld>
2017-08-02 16:17:09 -0400

    figure out file reading

--- util/generate_permutations.py
+++ util/generate_permutations.py
@@ -5,20 +5,26 @@ import json
 from argparse import ArgumentParser
 from pprint import pprint
 from itertools import product
+import sys
 
 
-def parse_args():
-    parser = ArgumentParser(description='Create course permutations')
-    parser.add_argument('--fields', action='', nargs=3)
+# # def parse_args():
+# parser = ArgumentParser(description='Create course permutations')
+# parser.add_argument('--fields', action='', nargs=3)
+#
+# # def generate_course_permutations():
+# with open('permutations.json') as permutation_options:
+#     permutation_data = json.load(permutation_options)
+#
+# start_dates = permutation_data["permutation_data"]["start"]
+# availability = permutation_data["permutation_data"]['availability']
+#
+# permutation_generation = [start_dates, availability]
+#
+# print list(product(*permutation_generation))
+
+with open(sys.argv[1], 'r') as permutation_file:
+
+    print permutation_file.read()[2]
 
 
-def generate_course_permutations():
-    with open('permutations.json') as permutation_options:
-        permutation_data = json.load(permutation_options)
-
-    start_dates = permutation_data["permutation_data"]["start"]
-    availability = permutation_data["permutation_data"]['availability']
-
-    permutation_generation = [start_dates, availability]
-
-    print list(product(*permutation_generation))
