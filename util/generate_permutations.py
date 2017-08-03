055168bcdb90882861ccfe357fe07eb5a64c616c
Shirley He <shirleyhe@C02T53Z1FVH4.tld>
2017-08-03 16:01:09 -0400

    read file from command line

--- util/generate_permutations.py
+++ util/generate_permutations.py
@@ -8,27 +8,27 @@ from pprint import pprint
 from itertools import product
 import sys
 
-
 # # def parse_args():
 # parser = ArgumentParser(description='Create course permutations')
 # parser.add_argument('--fields', action='', nargs=3)
 #
-def generate_permutations():
-
-    class Permutations(argparse.Action):
-        def __call__(self, parser, args, values, option_string=None):
-            setattr(args, self.dest, values)
-            return Permutations
+# def generate_permutations():
+#
+#     class Permutations(argparse.Action):
+#         def __call__(self, parser, args, values, option_string=None):
+#             setattr(args, self.dest, values)
+#             return Permutations
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--fields', action='generate_permutations', nargs=3)
-            args = parser.parse_args()
-            parser.register('action', 'generate_permutations', GenerationPermutations)
+#parser.add_argument('--fields', action='generate_permutations', nargs=3)
+parser.add_argument('filename')
+args = parser.parse_args()
 
-            with open(args.filename) as file:
-                permutation_data = json.load(file)
-                start_dates = permutation_data["permutation_data"]["start"]
-                availability = permutation_data["permutation_data"]['availability']
-                permutation_generation = [start_dates, availability]
-                print list(product(*permutation_generation))
+# parser.register('action', 'generate_permutations', GenerationPermutations)
+with open(args.filename) as file:
+    permutation_data = json.load(file)
+    start_dates = permutation_data["permutation_data"]["start"]
+    availability = permutation_data["permutation_data"]['availability']
+    permutation_generation = [start_dates, availability]
+    print list(product(*permutation_generation))
