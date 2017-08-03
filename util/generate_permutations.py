ce2ab9ee0a2fced58fdb62f916e9a0efcc926cef
Shirley He <shirleyhe@C02T53Z1FVH4.tld>
2017-08-03 11:59:39 -0400

    include permutations class

--- util/generate_permutations.py
+++ util/generate_permutations.py
@@ -2,6 +2,7 @@
 # other comment about how this file works, plus probably an example of implementation
 
 import json
+import argparse
 from argparse import ArgumentParser
 from pprint import pprint
 from itertools import product
@@ -12,19 +13,22 @@ import sys
 # parser = ArgumentParser(description='Create course permutations')
 # parser.add_argument('--fields', action='', nargs=3)
 #
-# # def generate_course_permutations():
-# with open('permutations.json') as permutation_options:
-#     permutation_data = json.load(permutation_options)
-#
-# start_dates = permutation_data["permutation_data"]["start"]
-# availability = permutation_data["permutation_data"]['availability']
-#
-# permutation_generation = [start_dates, availability]
-#
-# print list(product(*permutation_generation))
+def generate_permutations():
 
-with open(sys.argv[1], 'r') as permutation_file:
-
-    print permutation_file.read()[2]
+    class Permutations(argparse.Action):
+        def __call__(self, parser, args, values, option_string=None):
+            setattr(args, self.dest, values)
+            return Permutations
 
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--fields', action='generate_permutations', nargs=3)
+            args = parser.parse_args()
+            parser.register('action', 'generate_permutations', GenerationPermutations)
+
+            with open(args.filename) as file:
+                permutation_data = json.load(file)
+                start_dates = permutation_data["permutation_data"]["start"]
+                availability = permutation_data["permutation_data"]['availability']
+                permutation_generation = [start_dates, availability]
+                print list(product(*permutation_generation))
