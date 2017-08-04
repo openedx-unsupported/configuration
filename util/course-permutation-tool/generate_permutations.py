036f680597b3df14d37ed1c7c4296cc741e0937a
Shirley He <shirleyhe@C02T53Z1FVH4.tld>
2017-08-04 11:34:04 -0400

    create field dict for naming

--- util/course-permutation-tool/generate_permutations.py
+++ util/course-permutation-tool/generate_permutations.py
@@ -8,7 +8,6 @@ from pprint import pprint
 from itertools import product
 import sys
 
-
 def parse_field_arguments():
     parser = argparse.ArgumentParser()
     parser.add_argument('--fields', nargs="*", action="append", default=None,
@@ -16,14 +15,18 @@ def parse_field_arguments():
     parser.add_argument('filename')
 
     num_args = parser.parse_args()
-    if (len(num_args.fields[0]) > 3):
+    field_length = len(num_args.fields[0])
+    if (field_length > 3):
         raise argparse.ArgumentTypeError("--fields can only take a max of 3 values")
 
     file = open(num_args.filename)
     permutation_data = json.load(file)
-    first_field = permutation_data
-    second_field = permutation_data
-    third_field = permutation_data
+
+    fields = {}
+
+    # first_field = permutation_data
+    # second_field = permutation_data
+    # third_field = permutation_data
 
     # if no field arguments are given, just print out default data
     if not num_args.fields:
@@ -31,17 +34,15 @@ def parse_field_arguments():
         print default_permutation
     else:
         for permutation_choices in num_args.fields:
-            first_field = first_field["permutation_data"][permutation_choices[0]]
-            second_field = second_field["permutation_data"][permutation_choices[1]]
-            third_field = third_field["permutation_data"][permutation_choices[2]]
-
-    return first_field, second_field, third_field
+            for i in range(0, field_length):
+                fields[permutation_choices[i]] = permutation_data["permutation_data"][permutation_choices[i]]
+    return fields
 
 
 def generate_permutations(fields):
     #permutation_generation = [first_field, second_field, third_field]
-    # print list(product(*fields))
-    print ""
+     # print list(product(*fields))
+     print fields
 
 
 if __name__ == "__main__":
