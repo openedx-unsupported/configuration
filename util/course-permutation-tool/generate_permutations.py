5b0ce9a9a84fa7ddd8c1278a34b051273c088c02
Shirley He <shirleyhe@C02T53Z1FVH4.tld>
2017-08-04 16:33:46 -0400

    create output file

--- util/course-permutation-tool/generate_permutations.py
+++ util/course-permutation-tool/generate_permutations.py
@@ -6,7 +6,9 @@ import argparse
 from argparse import ArgumentParser
 from pprint import pprint
 from itertools import product
+from itertools import permutations
 import sys
+
 
 def parse_field_arguments():
     parser = argparse.ArgumentParser()
@@ -15,34 +17,47 @@ def parse_field_arguments():
     parser.add_argument('filename')
 
     num_args = parser.parse_args()
-    field_length = len(num_args.fields[0])
-    if (field_length > 3):
-        raise argparse.ArgumentTypeError("--fields can only take a max of 3 values")
 
     file = open(num_args.filename)
-    permutation_data = json.load(file)
+    file_data = json.load(file)
+
+    default_data = file_data["default_data"]
+    permutation_data = file_data["permutation_data"]
+
+    default_data_keys = permutation_data.keys()
+
+    # print default_data_keys
 
     fields = {}
 
-    # first_field = permutation_data
-    # second_field = permutation_data
-    # third_field = permutation_data
-
     # if no field arguments are given, just print out default data
     if not num_args.fields:
-        default_permutation = permutation_data["default_data"]
-        print default_permutation
+        default_permutation = file_data["default_data"]
+        fields = default_permutation
     else:
+        field_length = len(num_args.fields[0])
+
+        if (field_length > 3):
+            raise argparse.ArgumentTypeError("--fields can only take a max of 3 values")
+
         for permutation_choices in num_args.fields:
             for i in range(0, field_length):
-                fields[permutation_choices[i]] = permutation_data["permutation_data"][permutation_choices[i]]
+                fields[permutation_choices[i]] = permutation_data[permutation_choices[i]]
+            # for j in range(field_length, total_num_fields):
+            #     fields[permutation_choices[j]] =
+            # print permutation_data.keys() not in permutation_choices
+
     return fields
 
 
 def generate_permutations(fields):
-    #permutation_generation = [first_field, second_field, third_field]
-     # print list(product(*fields))
-     print fields
+
+    field_permutations = list(product(*fields.values()))
+
+    # make JSON output file
+    with open("test_courses.json", "w") as outfile:
+        json.dump(field_permutations, outfile)
+
 
 
 if __name__ == "__main__":
