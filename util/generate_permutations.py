dc8b033d628392d5aea602c215bb92ded9d7114f
Shirley He <shirleyhe@C02T53Z1FVH4.tld>
2017-08-04 09:56:41 -0400

    add default data handling

--- util/generate_permutations.py
+++ util/generate_permutations.py
@@ -14,22 +14,29 @@ import sys
 # parser.add_argument('--fields', action='', nargs=3)
 #
 def generate_permutations():
-
     parser = argparse.ArgumentParser()
-    parser.add_argument('--fields', nargs=3, action="append",
+    parser.add_argument('--fields', nargs=3, action="append", default=None,
                         help="Specify which fields to generate permutations on")
     parser.add_argument('filename')
     args = parser.parse_args()
 
     file = open(args.filename)
+    permutation_data = json.load(file)
+
+    # if no field arguments are given, just print out default data
+    if not args.fields:
+        default_permutation = permutation_data["default_data"]
+        print default_permutation
+    else:
+        for permutation_choices in args.fields:
+            first_field = permutation_data["permutation_data"][permutation_choices[0]]
+            # print first_field
+            second_field = permutation_data["permutation_data"][permutation_choices[1]]
+            permutation_generation = [first_field, second_field]
+            print list(product(*permutation_generation))
 
 
-    for permutation_choices in args.fields:
-        permutation_data = json.load(file)
-        first_field = permutation_data["permutation_data"][permutation_choices[0]]
-        second_field = permutation_data["permutation_data"][permutation_choices[1]]
-        permutation_generation = [first_field, second_field]
-        print list(product(*permutation_generation))
+
 
 
 if __name__ == "__main__":
