f463ce59a8380d2953b2b973dbfc3b08d3c51642
Shirley He <shirleyhe@C02T53Z1FVH4.tld>
2017-08-03 20:18:19 -0400

    figure out permutations command line input

--- util/generate_permutations.py
+++ util/generate_permutations.py
@@ -8,27 +8,29 @@ from pprint import pprint
 from itertools import product
 import sys
 
+
 # # def parse_args():
 # parser = ArgumentParser(description='Create course permutations')
 # parser.add_argument('--fields', action='', nargs=3)
 #
-# def generate_permutations():
-#
-#     class Permutations(argparse.Action):
-#         def __call__(self, parser, args, values, option_string=None):
-#             setattr(args, self.dest, values)
-#             return Permutations
+def generate_permutations():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--fields', nargs=3, action="append",
+                        help="Specify which fields to generate permutations on")
+    parser.add_argument('filename')
+    args = parser.parse_args()
+
+    file = open(args.filename)
 
 
-parser = argparse.ArgumentParser()
-#parser.add_argument('--fields', action='generate_permutations', nargs=3)
-parser.add_argument('filename')
-args = parser.parse_args()
+    for permutation_choices in args.fields:
+        permutation_data = json.load(file)
+        first_field = permutation_data["permutation_data"][permutation_choices[0]]
+        second_field = permutation_data["permutation_data"][permutation_choices[1]]
+        permutation_generation = [first_field, second_field]
+        print list(product(*permutation_generation))
 
-# parser.register('action', 'generate_permutations', GenerationPermutations)
-with open(args.filename) as file:
-    permutation_data = json.load(file)
-    start_dates = permutation_data["permutation_data"]["start"]
-    availability = permutation_data["permutation_data"]['availability']
-    permutation_generation = [start_dates, availability]
-    print list(product(*permutation_generation))
+
+if __name__ == "__main__":
+    generate_permutations()
