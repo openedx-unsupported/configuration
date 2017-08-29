4997b7c94af40b109f087ab27f3c61e510921c77
Feanil Patel <feanil@edx.org>
2017-08-29 14:20:52 -0400

    Add a default cluster key and other defaults.
    This is needed so that we can properly build devstacks without passing in any other config.

--- util/jenkins/ansible-provision.sh
+++ util/jenkins/ansible-provision.sh
@@ -127,9 +127,9 @@ fi
 
 if [[ -z $ami ]]; then
   if [[ $server_type == "full_edx_installation" ]]; then
-    ami="ami-c20128d4"
+    ami="ami-aa2f25d1"
   elif [[ $server_type == "ubuntu_16.04" || $server_type == "full_edx_installation_from_scratch" ]]; then
-    ami="ami-20631a36"
+    ami="ami-1d4e7a66"
   fi
 fi
 
