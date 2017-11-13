65e30a5d1f521803d4b0bb268aaf578ef4f8c054
Dmitry Gamanenko <dmitry.gamanenko@raccoongang.com>
2017-11-13 13:08:57 +0200

    Add edx_ansible_source_repo to ansible-bootstrap script.

--- util/install/ansible-bootstrap.sh
+++ util/install/ansible-bootstrap.sh
@@ -151,7 +151,7 @@ if [[ "true" == "${RUN_ANSIBLE}" ]]; then
     make requirements
 
     cd "${CONFIGURATION_DIR}"/playbooks/edx-east
-    "${PYTHON_BIN}"/ansible-playbook edx_ansible.yml -i '127.0.0.1,' -c local -e "configuration_version=${CONFIGURATION_VERSION}"
+    "${PYTHON_BIN}"/ansible-playbook edx_ansible.yml -i '127.0.0.1,' -c local -e "configuration_version=${CONFIGURATION_VERSION}" -e "edx_ansible_source_repo=${CONFIGURATION_REPO}"
 
     # cleanup
     rm -rf "${ANSIBLE_DIR}"
