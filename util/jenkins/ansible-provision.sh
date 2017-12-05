b3268a6000ddd1fd2f8c3ef20c32dca6f782e173
Feanil Patel <feanil@edx.org>
2017-12-05 17:30:06 -0500

    Update the base AMI with new credentials role.
    Credentials role was revamped, build a new ame based on that ansible.

--- util/jenkins/ansible-provision.sh
+++ util/jenkins/ansible-provision.sh
@@ -127,9 +127,9 @@ fi
 
 if [[ -z $ami ]]; then
   if [[ $server_type == "full_edx_installation" ]]; then
-    ami="ami-8609a6fc"
+    ami="ami-0d600177"
   elif [[ $server_type == "ubuntu_16.04" || $server_type == "full_edx_installation_from_scratch" ]]; then
-    ami="ami-da05a4a0"
+    ami="ami-aa2ea6d0"
   fi
 fi
 
