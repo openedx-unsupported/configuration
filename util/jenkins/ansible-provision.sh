1f7bce4b49b054cc066b2fab26335570cfb02201
nadeemshahzad <nshahzad@edx.org>
2017-12-11 16:03:51 +0500

    update base AMI

--- util/jenkins/ansible-provision.sh
+++ util/jenkins/ansible-provision.sh
@@ -127,7 +127,7 @@ fi
 
 if [[ -z $ami ]]; then
   if [[ $server_type == "full_edx_installation" ]]; then
-    ami="ami-0d600177"
+    ami="ami-d17c14ab"
   elif [[ $server_type == "ubuntu_16.04" || $server_type == "full_edx_installation_from_scratch" ]]; then
     ami="ami-aa2ea6d0"
   fi
