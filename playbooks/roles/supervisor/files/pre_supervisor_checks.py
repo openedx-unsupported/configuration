ef1bc02aee9d2b7e82f3817774c216e4b5ec7692
Feanil Patel <feanil@edx.org>
2018-03-02 11:06:25 -0500

    Go back in time and use the old syntax for everything over here.

--- playbooks/roles/supervisor/files/pre_supervisor_checks.py
+++ playbooks/roles/supervisor/files/pre_supervisor_checks.py
@@ -13,12 +13,12 @@ import time
 MIGRATION_COMMANDS = {
         'lms':     "/edx/bin/edxapp-migrate-lms --noinput --list",
         'cms':     "/edx/bin/edxapp-migrate-cms --noinput --list",
-        'xqueue':        ". {env_file}; sudo -E -u xqueue {python} {code_dir}/manage.py showmigrations",
-        'ecommerce':     ". {env_file}; sudo -E -u ecommerce {python} {code_dir}/manage.py showmigrations",
+        'xqueue':  "SERVICE_VARIANT=xqueue sudo -E -u xqueue {python} {code_dir}/manage.py migrate --noinput --list --settings=xqueue.aws_settings",
+        'ecommerce':     ". {env_file}; sudo -E -u ecommerce {python} {code_dir}/manage.py migrate --list",
         'insights':      ". {env_file}; sudo -E -u insights {python} {code_dir}/manage.py migrate --list",
-        'analytics_api': ". {env_file}; sudo -E -u analytics_api {python} {code_dir}/manage.py showmigrations",
-        'credentials':   ". {env_file}; sudo -E -u credentials {python} {code_dir}/manage.py showmigrations",
-        'discovery':     ". {env_file}; sudo -E -u discovery {python} {code_dir}/manage.py showmigrations",
+        'analytics_api': ". {env_file}; sudo -E -u analytics_api {python} {code_dir}/manage.py migrate --list",
+        'credentials':   ". {env_file}; sudo -E -u credentials {python} {code_dir}/manage.py migrate --list",
+        'discovery':     ". {env_file}; sudo -E -u discovery {python} {code_dir}/manage.py migrate --list",
     }
 HIPCHAT_USER = "PreSupervisor"
 
