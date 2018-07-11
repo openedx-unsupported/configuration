f29e393726ffb560ec92b3a733093fe5dfc75ea2
Ned Batchelder <ned@edx.org>
2018-07-11 15:21:58 -0400

    Leave a message for people still using the old name

---
+++ util/install/sandbox.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+##
+## This was the old name for native.sh.  If someone runs it, tell them
+## where the file went.
+##
+
+echo
+echo "The sandbox.sh script has been renamed to native.sh"
+echo "Let the author of your instructions know, or send us an email at oscm@edx.org"
+echo "Re-run your command with native.sh instead of sandbox.sh"
+echo
