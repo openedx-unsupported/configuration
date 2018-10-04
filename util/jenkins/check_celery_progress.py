799d164cba6ec2c46c62c10571c5ec9d67b294c6
Joseph Mulloy <jmulloy@edx.org>
2018-10-04 15:13:39 -0400

    Delete unneeded code

--- util/jenkins/check_celery_progress.py
+++ util/jenkins/check_celery_progress.py
@@ -2,8 +2,6 @@ import redis
 import json
 import datetime
 import click
-import boto3
-import botocore
 import backoff
 from itertools import zip_longest
 
@@ -73,37 +71,6 @@ class RedisWrapper(object):
         return self.redis.hmset(*args)
 
 
-class CwBotoWrapper(object):
-    def __init__(self):
-        self.client = boto3.client('cloudwatch')
-
-    @backoff.on_exception(backoff.expo,
-                          (botocore.exceptions.ClientError),
-                          max_tries=MAX_TRIES)
-    def list_metrics(self, *args, **kwargs):
-        return self.client.list_metrics(*args, **kwargs)
-
-    @backoff.on_exception(backoff.expo,
-                          (botocore.exceptions.ClientError),
-                          max_tries=MAX_TRIES)
-    def put_metric_data(self, *args, **kwargs):
-        #return self.client.put_metric_data(*args, **kwargs)
-        return True
-
-    @backoff.on_exception(backoff.expo,
-                          (botocore.exceptions.ClientError),
-                          max_tries=MAX_TRIES)
-    def describe_alarms_for_metric(self, *args, **kwargs):
-        return self.client.describe_alarms_for_metric(*args, **kwargs)
-
-    @backoff.on_exception(backoff.expo,
-                          (botocore.exceptions.ClientError),
-                          max_tries=MAX_TRIES)
-    def put_metric_alarm(self, *args, **kwargs):
-        #return self.client.put_metric_alarm(*args, **kwargs)
-        return True
-
-
 def datetime_from_str(string):
     return datetime.datetime.strptime(string, DATE_FORMAT)
 
@@ -161,57 +128,32 @@ def build_new_state(old_state, queue_first_items, current_time):
 @click.option('--environment', '-e', required=True)
 @click.option('--deploy', '-d', required=True,
               help="Deployment (i.e. edx or edge)")
-@click.option('--max-metrics', default=20,
-              help='Maximum number of CloudWatch metrics to publish')
-@click.option('--threshold', default=50,
-              help='Default maximum queue length before alarm notification is'
-              + ' sent')
+@click.option('--threshold', default=1,
+              help='Default queue maximum item age in minutes'
 @click.option('--queue-threshold', type=(str, int), multiple=True,
-              help='Threshold per queue in format --queue-threshold'
+              help='Per queue maximum item age in format --queue-threshold'
               + ' {queue_name} {threshold}. May be used multiple times')
-@click.option('--sns-arn', '-s', help='ARN for SNS alert topic', required=True)
 def check_queues(host, port, environment, deploy, max_metrics, threshold,
                  queue_threshold, sns_arn):
 
     thresholds = dict(queue_threshold)
 
     timeout = 1
-    namespace = "celery/{}-{}".format(environment, deploy)
     redis_client = RedisWrapper(host=host, port=port, socket_timeout=timeout,
                                 socket_connect_timeout=timeout)
     redis_client2 = RedisWrapper(host='localhost', port=port, socket_timeout=timeout,
                                 socket_connect_timeout=timeout)
-    cloudwatch = CwBotoWrapper()
-    metric_name = 'queue_length'
-    dimension = 'queue'
-##    response = cloudwatch.list_metrics(Namespace=namespace,
-##                                       MetricName=metric_name,
-##                                       Dimensions=[{'Name': dimension}])
-    existing_queues = []
-##    for m in response["Metrics"]:
-##        existing_queues.extend(
-##            [d['Value'] for d in m["Dimensions"] if (
-##                d['Name'] == dimension and
-##                not d['Value'].endswith(".pidbox") and
-##                not d['Value'].startswith("_kombu"))])
-
-#    print("keys {}".format(redis_client.keys()))
-    redis_queues = set([k.decode() for k in redis_client.keys()
+    queue_names = set([k.decode() for k in redis_client.keys()
                         if (redis_client.type(k) == b'list' and
                             not k.decode().endswith(".pidbox") and
                             not k.decode().startswith("_kombu"))])
 
-    all_queues = existing_queues + list(
-        set(redis_queues).difference(existing_queues)
-    )
     queue_age_hash = redis_client2.hgetall(QUEUE_AGE_HASH_NAME)
     old_state = unpack_state(queue_age_hash)
 
-    metric_data = []
-
     queue_first_items = {}
     current_time = datetime.datetime.now()
-    for queue_name in all_queues:
+    for queue_name in queue_names:
         queue_first_items[queue_name] = redis_client.lindex(queue_name, 0)
 
     print("old state {}".format(old_state))
@@ -221,64 +163,3 @@ def check_queues(host, port, environment, deploy, max_metrics, threshold,
 
     redis_client2.delete(QUEUE_AGE_HASH_NAME)
     redis_client2.hmset(QUEUE_AGE_HASH_NAME, pack_state(new_state))
-
-    for queue_name in all_queues:
-        metric_data.append({
-            'MetricName': metric_name,
-            'Dimensions': [{
-                "Name": dimension,
-                "Value": queue_name
-            }],
-            'Value': redis_client.llen(queue_name)
-        })
-
-    if len(metric_data) > 0:
-        for metric_data_grouped in grouper(metric_data, max_metrics):
-            print("metric_data {}".format(metric_data))
-            #cloudwatch.put_metric_data(Namespace=namespace, MetricData=metric_data)
-
-    for queue in all_queues:
-        dimensions = [{'Name': dimension, 'Value': queue}]
-        queue_threshold = threshold
-        if queue in thresholds:
-            queue_threshold = thresholds[queue]
-        # Period is in seconds
-        period = 60
-        evaluation_periods = 15
-        comparison_operator = "GreaterThanThreshold"
-        treat_missing_data = "notBreaching"
-        statistic = "Maximum"
-        actions = [sns_arn]
-        alarm_name = "{}-{} {} queue length over threshold".format(environment,
-                                                                   deploy,
-                                                                   queue)
-
-        print('Creating or updating alarm "{}"'.format(alarm_name))
-        cloudwatch.put_metric_alarm(AlarmName=alarm_name,
-                                    AlarmDescription=alarm_name,
-                                    Namespace=namespace,
-                                    MetricName=metric_name,
-                                    Dimensions=dimensions,
-                                    Period=period,
-                                    EvaluationPeriods=evaluation_periods,
-                                    TreatMissingData=treat_missing_data,
-                                    Threshold=queue_threshold,
-                                    ComparisonOperator=comparison_operator,
-                                    Statistic=statistic,
-                                    InsufficientDataActions=actions,
-                                    OKActions=actions,
-                                    AlarmActions=actions)
-
-# Stolen right from the itertools recipes
-# https://docs.python.org/3/library/itertools.html#itertools-recipes
-def grouper(iterable, n, fillvalue=None):
-    "Collect data into fixed-length chunks or blocks"
-    # grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx"
-    args = [iter(iterable)] * n
-    chunks = zip_longest(*args, fillvalue=fillvalue)
-    # Remove Nones in function
-    for chunk in chunks:
-        yield [v for v in chunk if v is not None]
-
-if __name__ == '__main__':
-    check_queues()
