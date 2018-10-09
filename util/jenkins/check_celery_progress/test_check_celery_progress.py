a3fba712b23d67d57f54f3f0c005152057847280
Joseph Mulloy <jmulloy@edx.org>
2018-10-09 16:06:41 -0400

    Refactor and add tests for should_send_alert

--- util/jenkins/check_celery_progress/test_check_celery_progress.py
+++ util/jenkins/check_celery_progress/test_check_celery_progress.py
@@ -21,6 +21,8 @@ class TestCheckCeleryQueues(unittest.TestCase):
     time_0 = datetime_from_str("2018-10-04 11:00:51.111367")
     time_1_min = datetime_from_str("2018-10-04 11:01:51.111367")
 
+    threshold = 5 * 60
+
     def test_equal_output_if_queues_stuck(self):
         state_0 = build_new_state({}, self.queue_first_items_0, self.time_0)
         state_1 = build_new_state(state_0, self.queue_first_items_0, self.time_1_min)
@@ -33,16 +35,26 @@ class TestCheckCeleryQueues(unittest.TestCase):
         self.assertEqual(state_1['edx.lms.core.default']['first_occurance_time'], self.time_1_min)
         self.assertEqual(state_1['edx.lms.core.default']['correlation_id'], "27d76be4-408b-4f5b-a132-c0ffee465e90")
 
-    def test_should_send_alert(self):
-        current_time = self.time_0
-        first_occurance_time = current_time - timedelta(minutes=6)
-        threshold = 5 * 60
-        result = should_send_alert(first_occurance_time, current_time, threshold)
+    def test_should_send_alert_0_delta(self):
+        first_occurance_time = self.time_0
+        result = should_send_alert(first_occurance_time, self.time_0, self.threshold)
+        self.assertEqual(False, result)
+
+    def test_should_send_alert_under_threshold(self):
+        first_occurance_time = self.time_0 - timedelta(minutes=4)
+        result = should_send_alert(first_occurance_time, self.time_0, self.threshold)
+        self.assertEqual(False, result)
+
+    def test_should_send_alert_over_threshold(self):
+        first_occurance_time = self.time_0 - timedelta(minutes=6)
+        result = should_send_alert(first_occurance_time, self.time_0, self.threshold)
         self.assertEqual(True, result)
 
-        first_occurance_time = current_time - timedelta(minutes=4)
-        result = should_send_alert(first_occurance_time, current_time, threshold)
+    def test_should_send_alert_negative_delta(self):
+        first_occurance_time = self.time_0 + timedelta(minutes=4)
+        result = should_send_alert(first_occurance_time, self.time_0, self.threshold)
         self.assertEqual(False, result)
+
 
 if __name__ == '__main__':
     unittest.main()
