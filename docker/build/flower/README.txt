b823e227564cdf7ea47d9021cce1b661e219c4a4
Cory Lee <cory@edx.org>
2019-01-29 15:15:02 -0500

    Flower docker image

---
+++ docker/build/flower/README.txt
@@ -0,0 +1,3 @@
+Example:
+docker build . -t edxops/flower:latest
+docker run -it --rm -p 127.0.0.1:5555:5555 edxops/flower:latest flower --broker=redis://:@some-redis-url.com:6379 --conf=flowerconfig.py
---
+++ docker/build/flower/flowerconfig.py
@@ -0,0 +1,8 @@
+address             = "0.0.0.0"
+port                =  5555
+
+# Oauth would be added like so:
+# oauth2_key          = "xxxxx"
+# oauth2_secret       = "yyyyy"
+# oauth2_redirect_uri = "http://url_of_flower:5555/login"
+# auth                = ".*@edx.org"
