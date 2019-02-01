276664f3b6f0f7549c22115ced6ac9b2cc7390db
Cory Lee <cory@edx.org>
2019-02-01 15:21:35 -0500

    Fixing config and README

--- docker/build/flower/flowerconfig.py
+++ docker/build/flower/flowerconfig.py
@@ -1,8 +1,10 @@
-address             = "0.0.0.0"
-port                =  5555
 
-# Oauth would be added like so:
-# oauth2_key          = "xxxxx"
-# oauth2_secret       = "yyyyy"
-# oauth2_redirect_uri = "http://url_of_flower:5555/login"
-# auth                = ".*@edx.org"
+import os
+
+address = os.getenv('ADDRESS', "0.0.0.0")
+port = os.getenv('PORT', 5555)
+
+oauth2_key = os.getenv('OAUTH2_KEY', None)
+oauth2_secret = os.getenv('OAUTH2_SECRET', None)
+oauth2_redirect_uri = os.getenv('OAUTH2_REDIRECT_URI', None)
+auth = os.getenv('AUTH', None)
