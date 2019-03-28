b75862dbf6f9d36c4ed0c7f393e414d05c305604
syedimranhassan <imran.hassan@arbisoft.com>
2019-03-28 17:05:04 +0500

    for testing

--- util/jenkins/test.py
+++ util/jenkins/test.py
@@ -4,7 +4,9 @@ import click
 @click.command()
 @click.option('--username', envvar='USERNAME')
 def great(username):
+    print "++++++++++++++++++++"
     print username
+    print "+++++++++++++++++++++"
 
 
 if __name__ == '__main__':
