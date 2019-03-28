87eb0dde0df92958ee2fc80863416e3f0d11a5c9
syedimranhassan <imran.hassan@arbisoft.com>
2019-03-28 17:44:32 +0500

    for testing

--- util/jenkins/test.py
+++ util/jenkins/test.py
@@ -3,9 +3,11 @@ import click
 
 @click.command()
 @click.option('--username', envvar='USERNAME')
+@click.option('--password', envvar='PASSWORD')
 def great(username):
     print "++++++++++++++++++++"
     print username
+    print password
     print "+++++++++++++++++++++"
 
 
