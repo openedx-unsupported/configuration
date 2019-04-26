c80d09dda60b9339af3a59aca89577df139550b1
Stuart Young <syoung@edx.org>
2019-04-26 11:57:39 -0400

    stop runnin lettuce in sanity tests

--- playbooks/roles/test_build_server/files/test-development-environment.sh
+++ playbooks/roles/test_build_server/files/test-development-environment.sh
@@ -49,12 +49,6 @@ case "$1" in
         paver test_bokchoy -t lms/test_lms_matlab_problem.py::MatlabProblemTest --fasttest
         ;;
 
-    "lettuce")
-        # Run some of the lettuce acceptance tests
-        paver test_acceptance -s lms --extra_args="lms/djangoapps/courseware/features/problems.feature -s 1"
-        paver test_acceptance -s cms --extra_args="cms/djangoapps/contentstore/features/html-editor.feature -s 1" --fasttest
-        ;;
-
     "quality")
         # Generate quality reports
         paver run_quality
