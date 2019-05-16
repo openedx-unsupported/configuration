2ccb54778bdc98ee1e3d44662631ddfe006507ee
hamzamunir7300 <hamxa.muneer@gmail.com>
2019-05-16 16:40:18 +0500

    fixes for sandbox apros

--- util/jenkins/ansible-provision.sh
+++ util/jenkins/ansible-provision.sh
@@ -212,7 +212,8 @@ cd playbooks
 cat << EOF > $extra_vars_file
 edx_platform_version: $edxapp_version
 forum_version: $forum_version
-forum_ruby_version: '2.4.1'
+forum_ruby_version: '2.3.7'
+mcka_apros_ruby_version: '2.3.7'
 forum_source_repo: 'https://github.com/edx-solutions/cs_comments_service.git'
 ansible_distribution: 'Ubuntu'
 ansible_distribution_release: 'xenial'
