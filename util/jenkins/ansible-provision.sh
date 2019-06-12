46721b53eea171d22522821c55ae16c5ad364641
hamzamunir7300 <hamxa.muneer@gmail.com>
2019-06-12 16:58:13 +0500

    changes

--- util/jenkins/ansible-provision.sh
+++ util/jenkins/ansible-provision.sh
@@ -595,7 +595,7 @@ cd $WORKSPACE/ansible-private
 ansible-playbook -vvvv -i "${deploy_host}," mckinsey-create-dbs.yml $extra_var_arg --user ubuntu
 cd $WORKSPACE/configuration/playbooks/edx-east
 
-ansible-playbook -vvvv -i "${deploy_host}," mckinseysandbox.yml  $extra_var_arg --user ubuntu
+ansible-playbook -vvvv -i "${deploy_host}," mckinseyapros.yml  $extra_var_arg --user ubuntu
 
 
 extra_var_arg+=' -e migrate_db="yes"'
@@ -605,8 +605,8 @@ cd $WORKSPACE/configuration/playbooks/edx-east
 
 #git checkout $ForumConfigurationVersion
 
-#ansible-playbook -vvvv mongo_3_2.yml -i "${deploy_host},"  $extra_var_arg --user ubuntu
-#ansible-playbook -vvvv forum.yml -i "${deploy_host}," $extra_var_arg --user ubuntu
+ansible-playbook -vvvv mongo_3_2.yml -i "${deploy_host},"  $extra_var_arg --user ubuntu
+ansible-playbook -vvvv forum.yml -i "${deploy_host}," $extra_var_arg --user ubuntu
 
 PATTERN='all'
 ansible ${PATTERN} -i "${deploy_host}," -u ubuntu -m shell -a 'sudo -u www-data /edx/app/edxapp/venvs/edxapp/bin/python /edx/app/edxapp/edx-platform/manage.py lms migrate --settings aws --noinput'
