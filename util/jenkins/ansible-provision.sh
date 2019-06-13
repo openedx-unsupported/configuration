81424055ccd3aafd03b2769c3d30678b90cee64f
hamzamunir7300 <hamxa.muneer@gmail.com>
2019-06-13 16:46:35 +0500

    changes

--- util/jenkins/ansible-provision.sh
+++ util/jenkins/ansible-provision.sh
@@ -98,6 +98,7 @@ rm -rf private_vars/
 git clone https://hamzamunir7300:hamza123@github.com/hamzamunir7300/private_vars.git
 chmod -R 0777 private_vars/
 private_vars_file="${WORKSPACE}/private_vars/top_secret.yml"
+rm -rf ansible-private/
 git clone https://hamzamunir7300:hamza123@github.com/mckinseyacademy/mcka-ansible.git ansible-private
 cd $WORKSPACE/configuration
 pip install -r requirements.txt
