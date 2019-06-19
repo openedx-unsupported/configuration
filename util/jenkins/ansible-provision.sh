02bd52ed35b15af1406063c7d2dc3182d8823f2c
hamzamunir7300 <hamxa.muneer@gmail.com>
2019-06-19 17:41:50 +0500

    changing order of roles

--- util/jenkins/ansible-provision.sh
+++ util/jenkins/ansible-provision.sh
@@ -592,6 +592,7 @@ cd $WORKSPACE/ansible-private
 
 #IpAddress=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$InstanceNameTag" --output text --query 'Reservations[*].Instances[*].[PrivateIpAddress]')
 
+run_ansible -vvvv -i "${deploy_host}," $WORKSPACE/configuration/playbooks/edx-east/mysql.yml $extra_var_arg --user ubuntu
 run_ansible -vvvv -i "${deploy_host}," mckinsey-create-dbs.yml $extra_var_arg --user ubuntu
 
 run_ansible -vvvv $WORKSPACE/configuration/playbooks/edx-east/mckinseysandbox.yml -i "${deploy_host}," $extra_var_arg --user ubuntu
