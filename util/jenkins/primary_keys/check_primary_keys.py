91982348375eefcbd52d11ade613d1904f07604d
syedimranhassan <imran.hassan@arbisoft.com>
2019-06-25 12:17:23 +0500

    Added whitelist and email

--- util/jenkins/primary_keys/check_primary_keys.py
+++ util/jenkins/primary_keys/check_primary_keys.py
@@ -282,8 +282,8 @@ def controller(username, password, environment, deploy, rdsignore):
     rds_list = get_rds_from_all_regions()
     filtered_rds_list = list(filter(lambda x: x['name'] not in rdsignore, rds_list))
     table_list = check_primary_keys(filtered_rds_list, username, password, environment, deploy)
-    if len(table_list) > 0:
-        print(table_list)
+    #if len(table_list) > 0:
+    print(table_list)
         #send_an_email("ihassan@edx.org", "daemon@edx.org", table_list, "us-east-1")
     sys.exit(0)
 
