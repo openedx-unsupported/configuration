3764ca4b2e18bedf53531e84596fd1fa9ef68088
Stuart Young <syoung@edx.org>
2019-08-20 15:43:32 -0400

    fixup

--- playbooks/roles/test_build_server/files/test-development-environment.sh
+++ playbooks/roles/test_build_server/files/test-development-environment.sh
@@ -30,7 +30,7 @@ case "$1" in
         # Generate some coverage reports
         # Since `TEST_SUITE` is not set, change the coverage file written by the
         # previous test to a generic one.
-        cp reports/cms.coverage reports/.coverage
+        cp reports/common_lib_xmodule.coverage reports/.coverage
         paver coverage
 
         # Run some of the djangoapp unit tests
