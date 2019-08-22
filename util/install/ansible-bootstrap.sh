89156e8f0591c5fc4f506d3e247b5c67c0978113
Makavura <makavuramughanga@gmail.com>
2019-08-22 10:59:39 +0300

    redirect source to github.com/Makavura/configuration

--- util/install/ansible-bootstrap.sh
+++ util/install/ansible-bootstrap.sh
@@ -9,7 +9,7 @@
 # Can be run as follows:
 #
 # UPGRADE_OS=true CONFIGURATION_VERSION="master" \
-# bash <(curl -s https://raw.githubusercontent.com/edx/configuration/master/util/install/ansible-bootstrap.sh)
+# bash <(curl -s https://raw.githubusercontent.com/Makavura/configuration/master/util/install/ansible-bootstrap.sh)
 
 set -xe
 
@@ -22,7 +22,7 @@ if [[ -z "${ANSIBLE_VERSION}" ]]; then
 fi
 
 if [[ -z "${CONFIGURATION_REPO}" ]]; then
-  CONFIGURATION_REPO="https://github.com/edx/configuration.git"
+  CONFIGURATION_REPO="https://github.com/Makavura/configuration.git"
 fi
 
 if [[ -z "${CONFIGURATION_VERSION}" ]]; then
