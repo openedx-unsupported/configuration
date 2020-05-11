fd935e7f98e00cca083fe0287418661095e0768b
Jeremy Bowman <jbowman@edx.org>
2020-05-11 10:23:50 -0400

    Upgrade setuptools, pip, and virtualenv (#5774)
    The versions of setuptools, pip, and virtualenv we specify here are getting a little dated, which is already causing incompatibility with recent pip-tools releases and has caused at least one installation failure during Juniper testing. We'd like to upgrade these before Juniper is released. Points worth noting:
    
    * virtualenv 20+ changed the default behavior to symlinking instead of copying the Python binary, which doesn't play nicely with our current secondary symlinks to that that we use in a few places. Sticking with 16.7.9 for now, maybe the new service containers won't even need those symlinks so not wasting time working around the issue.
    
    * setuptools 46.1.3 is the latest version as of this writing, but 44.1.0 is the latest release that still supports Python 2.7. For now I'm just setting us up to use 44.1.0 everywhere, since there are still a few corners where Ansible and such are running 2.7 (those will be fixed in later PRs).
    
    * pip 20.0.2 works with all recent versions of pip-tools, and avoids some breaking changes in 20.1 that we need more time to address.
    
    * virtualenvwrapper was being installed but seemed unused (I can't find anything outside of README files in the edx or edx-ops organizations that calls mkvirtualenv or workon), and there isn't a release yet that works with virtualenv>16.7.9.
    
    Successful sandbox build can be found at https://tools-edx-jenkins.edx.org/job/Sandboxes/job/CreateSandbox/29943/ .
    # Conflicts:
    #	docker/build/analytics_pipeline/Dockerfile
    #	docker/build/go-agent/Dockerfile
    #	util/install/ansible-bootstrap.sh

--- util/install/ansible-bootstrap.sh
+++ util/install/ansible-bootstrap.sh
@@ -40,9 +40,9 @@ fi
 #
 # Bootstrapping constants
 #
-VIRTUAL_ENV_VERSION="15.2.0"
-PIP_VERSION="9.0.3"
-SETUPTOOLS_VERSION="39.0.1"
+VIRTUAL_ENV_VERSION="16.7.10"
+PIP_VERSION="20.0.2"
+SETUPTOOLS_VERSION="44.1.0"
 VIRTUAL_ENV="/tmp/bootstrap"
 PYTHON_BIN="${VIRTUAL_ENV}/bin"
 PYTHON_VERSION="3.5"
@@ -76,10 +76,13 @@ then
 elif grep -q 'Xenial Xerus' /etc/os-release
 then
     SHORT_DIST="xenial"
+elif grep -q 'Bionic Beaver' /etc/os-release
+then
+    SHORT_DIST="bionic"
 else
     cat << EOF
 
-    This script is only known to work on Ubuntu Trusty and Xenial,
+    This script is only known to work on Ubuntu Trusty, Xenial, and Bionic;
     exiting.  If you are interested in helping make installation possible
     on other platforms, let us know.
 
@@ -91,6 +94,12 @@ EDX_PPA="deb http://ppa.edx.org ${SHORT_DIST} main"
 
 # Upgrade the OS
 apt-get update -y
+
+# To apt-key update in bionic, gnupg is needed.
+if [[ "${SHORT_DIST}" == bionic ]] ;then
+  apt-get install -y gnupg
+fi
+
 apt-key update -y
 
 if [ "${UPGRADE_OS}" = true ]; then
@@ -99,42 +108,41 @@ if [ "${UPGRADE_OS}" = true ]; then
 fi
 
 # Required for add-apt-repository
-apt-get install -y software-properties-common python-software-properties
+apt-get install -y software-properties-common
+if [[ "${SHORT_DIST}" != bionic ]] && [[ "${SHORT_DIST}" != xenial ]];then
+  apt-get install -y python-software-properties
+fi
 
 # Add git PPA
 add-apt-repository -y ppa:git-core/ppa
 
-# For older software we need to install our own PPA.
-apt-key adv --keyserver "${EDX_PPA_KEY_SERVER}" --recv-keys "${EDX_PPA_KEY_ID}"
-add-apt-repository -y "${EDX_PPA}"
-
-# Add deadsnakes repository for python3.5 usage in
-# Ubuntu versions different than xenial.
-if [[ "${SHORT_DIST}" != xenial ]] ;then
-  add-apt-repository -y ppa:deadsnakes/ppa
+# For older software we need to install our own PPA
+# Phased out with Ubuntu 18.04 Bionic
+if [[ "${SHORT_DIST}" != bionic ]] ;then
+  apt-key adv --keyserver "${EDX_PPA_KEY_SERVER}" --recv-keys "${EDX_PPA_KEY_ID}"
+  add-apt-repository -y "${EDX_PPA}"
 fi
+
 
 # Install python 2.7 latest, git and other common requirements
 # NOTE: This will install the latest version of python 2.7 and
 # which may differ from what is pinned in virtualenvironments
 apt-get update -y
 
-apt-get install -y python2.7 python2.7-dev python-pip python-apt python-yaml python-jinja2 build-essential sudo git-core libmysqlclient-dev libffi-dev libssl-dev
+apt-get install -y python2.7 python2.7-dev python-pip python-apt python-jinja2 build-essential sudo git-core libmysqlclient-dev libffi-dev libssl-dev
 
 
-apt-get install -y python${PYTHON_VERSION}-dev python3-pip python3-apt
-
-python${PYTHON_VERSION} -m pip install --upgrade pip=="${PIP_VERSION}"
+pip install --upgrade pip=="${PIP_VERSION}"
 
 # pip moves to /usr/local/bin when upgraded
 PATH=/usr/local/bin:${PATH}
-python${PYTHON_VERSION} -m pip install setuptools=="${SETUPTOOLS_VERSION}"
-python${PYTHON_VERSION} -m pip install virtualenv=="${VIRTUAL_ENV_VERSION}"
+pip install setuptools=="${SETUPTOOLS_VERSION}"
+pip install virtualenv=="${VIRTUAL_ENV_VERSION}"
 
 
 if [[ "true" == "${RUN_ANSIBLE}" ]]; then
     # create a new virtual env
-    /usr/local/bin/virtualenv --python=python${PYTHON_VERSION} "${VIRTUAL_ENV}"
+    /usr/local/bin/virtualenv "${VIRTUAL_ENV}"
 
     PATH="${PYTHON_BIN}":${PATH}
 
@@ -168,4 +176,3 @@ else
     mkdir -p /edx/ansible/facts.d
     echo '{ "ansible_bootstrap_run": true }' > /edx/ansible/facts.d/ansible_bootstrap.json
 fi
-
