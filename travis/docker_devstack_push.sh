b2829d41bbe5aac3c015c6725c615572c2107e32
Joseph Mulloy <jmulloy@edx.org>
2020-08-27 16:41:48 -0400

    fix

--- travis/docker_devstack_push.sh
+++ travis/docker_devstack_push.sh
@@ -8,7 +8,6 @@ tag_name=${TAG_NAME:-latest}
 image_tag=edxops/${APP_NAME}:${tag_name}
 
 # build the Dockerfile of the IDA defined by APP_NAME and tag it with its name; don't use cache
-cd configuration
 docker build -f docker/build/${APP_NAME}/Dockerfile --build-arg BASE_IMAGE_TAG=${tag_name} --build-arg OPENEDX_RELEASE=${openedx_release} -t ${image_tag} --no-cache .
 
 # push built image to DockerHub
