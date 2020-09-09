095c30e302ba0f120a5a88fc9aa1a0fe79e3b73c
nadeemshahzad <nshahzad@edx.org>
2020-09-09 12:33:04 +0500

    add support for ubuntu 20.04

--- util/install/ansible-bootstrap.sh
+++ util/install/ansible-bootstrap.sh
@@ -79,6 +79,9 @@ then
 elif grep -q 'Bionic Beaver' /etc/os-release
 then
     SHORT_DIST="bionic"
+elif grep -q 'Focal Fossa' /etc/os-release
+then
+    SHORT_DIST="focal"
 else
     cat << EOF
 
@@ -110,7 +113,7 @@ fi
 
 # Required for add-apt-repository
 apt-get install -y software-properties-common
-if [[ "${SHORT_DIST}" != bionic ]] && [[ "${SHORT_DIST}" != xenial ]];then
+if [[ "${SHORT_DIST}" != bionic ]] && [[ "${SHORT_DIST}" != xenial ]] && [[ "${SHORT_DIST}" != focal ]] ;then
   apt-get install -y python-software-properties
 fi
 
@@ -118,8 +121,8 @@ fi
 add-apt-repository -y ppa:git-core/ppa
 
 # For older software we need to install our own PPA
-# Phased out with Ubuntu 18.04 Bionic
-if [[ "${SHORT_DIST}" != bionic ]] ;then
+# Phased out with Ubuntu 18.04 Bionic and Ubuntu 20.04 Focal
+if [[ "${SHORT_DIST}" != bionic ]] && [[ "${SHORT_DIST}" != focal ]] ;then
   apt-key adv --keyserver "${EDX_PPA_KEY_SERVER}" --recv-keys "${EDX_PPA_KEY_ID}"
   add-apt-repository -y "${EDX_PPA}"
 fi
@@ -130,12 +133,10 @@ if [[ "${SHORT_DIST}" != xenial ]] ;then
   add-apt-repository -y ppa:deadsnakes/ppa
 fi
 
-# Install python 2.7 latest, git and other common requirements
-# NOTE: This will install the latest version of python 2.7 and
-# which may differ from what is pinned in virtualenvironments
+# Install git and other common requirements
 apt-get update -y
 
-apt-get install -y python2.7 python2.7-dev python-pip python-apt python-jinja2 build-essential sudo git-core libmysqlclient-dev libffi-dev libssl-dev
+apt-get install -y python3-jinja2 build-essential sudo git-core libmysqlclient-dev libffi-dev libssl-dev
 
 apt-get install -y python${PYTHON_VERSION}-dev python3-pip python3-apt
 
