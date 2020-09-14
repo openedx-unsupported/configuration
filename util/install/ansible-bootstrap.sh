9db20b2ccd83940a81c99ff73d6fccb8ccabe054
Nadeem Shahzad <nadem.shahzad@gmail.com>
2020-09-14 09:34:53 +0500

    Revert "add support for Ubuntu 20.04 (PSRE-203)"

--- util/install/ansible-bootstrap.sh
+++ util/install/ansible-bootstrap.sh
@@ -79,9 +79,6 @@ then
 elif grep -q 'Bionic Beaver' /etc/os-release
 then
     SHORT_DIST="bionic"
-elif grep -q 'Focal Fossa' /etc/os-release
-then
-    SHORT_DIST="focal"
 else
     cat << EOF
 
@@ -113,7 +110,7 @@ fi
 
 # Required for add-apt-repository
 apt-get install -y software-properties-common
-if [[ "${SHORT_DIST}" != bionic ]] && [[ "${SHORT_DIST}" != xenial ]] && [[ "${SHORT_DIST}" != focal ]] ;then
+if [[ "${SHORT_DIST}" != bionic ]] && [[ "${SHORT_DIST}" != xenial ]];then
   apt-get install -y python-software-properties
 fi
 
@@ -121,8 +118,8 @@ fi
 add-apt-repository -y ppa:git-core/ppa
 
 # For older software we need to install our own PPA
-# Phased out with Ubuntu 18.04 Bionic and Ubuntu 20.04 Focal
-if [[ "${SHORT_DIST}" != bionic ]] && [[ "${SHORT_DIST}" != focal ]] ;then
+# Phased out with Ubuntu 18.04 Bionic
+if [[ "${SHORT_DIST}" != bionic ]] ;then
   apt-key adv --keyserver "${EDX_PPA_KEY_SERVER}" --recv-keys "${EDX_PPA_KEY_ID}"
   add-apt-repository -y "${EDX_PPA}"
 fi
@@ -133,19 +130,21 @@ if [[ "${SHORT_DIST}" != xenial ]] ;then
   add-apt-repository -y ppa:deadsnakes/ppa
 fi
 
-# Install git and other common requirements
+# Install python 2.7 latest, git and other common requirements
+# NOTE: This will install the latest version of python 2.7 and
+# which may differ from what is pinned in virtualenvironments
 apt-get update -y
 
-apt-get install -y python3-jinja2 build-essential sudo git-core libmysqlclient-dev libffi-dev libssl-dev
+apt-get install -y python2.7 python2.7-dev python-pip python-apt python-jinja2 build-essential sudo git-core libmysqlclient-dev libffi-dev libssl-dev
 
-apt-get install -y python${PYTHON_VERSION} python${PYTHON_VERSION}-dev python3-pip python3-apt
+apt-get install -y python${PYTHON_VERSION}-dev python3-pip python3-apt
 
-pip3 install --upgrade pip=="${PIP_VERSION}"
+python${PYTHON_VERSION} -m pip install --upgrade pip=="${PIP_VERSION}"
 
 # pip moves to /usr/local/bin when upgraded
 PATH=/usr/local/bin:${PATH}
-pip3 install setuptools=="${SETUPTOOLS_VERSION}"
-pip3 install virtualenv=="${VIRTUAL_ENV_VERSION}"
+python${PYTHON_VERSION} -m pip install setuptools=="${SETUPTOOLS_VERSION}"
+python${PYTHON_VERSION} -m pip install virtualenv=="${VIRTUAL_ENV_VERSION}"
 
 if [[ "true" == "${RUN_ANSIBLE}" ]]; then
     # create a new virtual env
