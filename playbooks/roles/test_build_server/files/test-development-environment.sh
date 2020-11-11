2733a2714bb26939136e6cd2eeb15dc2bad90769
Aarif <mraarif@outlook.com>
2020-11-11 17:00:10 +0500

    Updated the jenkins worker Dockerfile to use ubuntu 20 as base image

--- playbooks/roles/test_build_server/files/test-development-environment.sh
+++ playbooks/roles/test_build_server/files/test-development-environment.sh
@@ -18,7 +18,7 @@ cd edx-platform-clone
 
 # This will run all of the setup it usually runs, but none of the
 # tests because TEST_SUITE isn't defined.
-export PYTHON_VERSION=3.5
+export PYTHON_VERSION=3.8
 source scripts/jenkins-common.sh
 
 case "$1" in
