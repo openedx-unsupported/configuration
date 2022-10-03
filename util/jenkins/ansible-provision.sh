2a454408993faf7704f7e3ab2afa1cd9e2c66812
nadeemshahzad <mshahzad@2u.com>
2022-10-03 11:15:40 +0500

    feat: install edx-exams on sandbox using docker

--- util/jenkins/ansible-provision.sh
+++ util/jenkins/ansible-provision.sh
@@ -655,5 +655,114 @@ if [[ $enable_newrelic == "true" ]]; then
     run_ansible run_role.yml -i "${deploy_host}," -e role=newrelic_infrastructure $extra_var_arg  --user ubuntu
 fi
 
+function provision_containerized_app() {
+    echo "#!/usr/bin/env bash"
+    echo "set -ex"
+
+    # Create app staticfiles dir
+    echo "mkdir /edx/var/${app_service_name}/staticfiles/ -p && chmod 777 /edx/var/${app_service_name} -R"
+
+    # Checkout code in app directory
+    echo "cd /edx/app/"
+    echo "git clone https://github.com/edx/${app_repo}.git"
+
+    # Replace deploy_host in app config file with sandbox DNS name
+    echo "sed -i 's/deploy_host/${dns_name}.${dns_zone}/g' /var/tmp/${app_service_name}.yml"
+
+    # Install yq for yaml processing 
+    echo "wget https://github.com/mikefarah/yq/releases/download/v4.27.5/yq_linux_amd64  -O /usr/bin/yq && chmod +x /usr/bin/yq"
+
+    # Combine app config with jwt_signature config
+    echo "yq eval-all '. as \$item ireduce ({}; . *+ \$item)' /var/tmp/${app_service_name}.yml  /tmp/lms_jwt_signature.yml > /edx/etc/${app_service_name}.yml"
+    
+    # Provision IDA User in LMS
+    echo "source /edx/app/edxapp/edxapp_env && python /edx/app/edxapp/edx-platform/manage.py lms --settings=production manage_user ${app_service_name}_worker ${app_service_name}_worker@example.com --staff --superuser"
+
+    # Create the DOT applications - one for single sign-on and one for backend service IDA-to-IDA authentication.
+    echo "source /edx/app/edxapp/edxapp_env && python /edx/app/edxapp/edx-platform/manage.py lms --settings=production create_dot_application --grant-type authorization-code --skip-authorization --redirect-uris 'https://${app_hostname}-${dns_name}.${dns_zone}/complete/edx-oauth2/' --client-id '${app_service_name}-sso-key' --client-secret '${app_service_name}-sso-secret' --scopes 'user_id' ${app_service_name}-sso ${app_service_name}_worker"
+    echo "source /edx/app/edxapp/edxapp_env && python /edx/app/edxapp/edx-platform/manage.py lms --settings=production create_dot_application --grant-type client-credentials --client-id '${app_service_name}-backend-service-key' --client-secret '${app_service_name}-backend-service-secret' ${app_service_name}-backend-service ${app_service_name}_worker"
+
+    # Checkout code version
+    echo "cd /edx/app/${app_repo}"
+    echo "git checkout ${app_version}"
+
+    # Create app database
+    echo "mysql -uroot -e \"CREATE DATABASE \\\`${app_service_name}\\\`;\""
+
+    # Build app docker image and run app container
+    echo "docker build . -t ${app_service_name}:latest"
+    echo "docker run --rm -it -d -e DJANGO_SETTINGS_MODULE=${app_service_name}.settings.production -e ${app_cfg}=/${app_service_name}.yml -v '/edx/app/${app_repo}:/edx/app/${app_repo}/' -v '/edx/etc/${app_service_name}.yml:/${app_service_name}.yml' -v '/edx/var/${app_service_name}/staticfiles/:/var/tmp/' --name ${app_service_name}.app --network=host --entrypoint '/bin/bash' ${app_service_name}:latest -c 'while true; do exec gunicorn --workers=2 --name ${app_service_name} -c /edx/app/${app_repo}/${app_service_name}/docker_gunicorn_configuration.py --log-file - --max-requests=1000 ${app_service_name}.wsgi:application; sleep 2; done'"
+
+    # Wait for app container
+    echo "sleep 5"
+
+    # Run migrations
+    echo "docker exec -t ${app_service_name}.app bash -c \"python3 manage.py migrate\""
+
+    # Run collectstatic
+    echo "docker exec -t ${app_service_name}.app bash -c \"python3 manage.py collectstatic --noinput\""
+ 
+     # Create superuser
+    echo "docker exec -t ${app_service_name}.app bash -c \"echo 'from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser(\\\"edx\\\", \\\"edx@example.com\\\", \\\"edx\\\") if not User.objects.filter(username=\\\"edx\\\").exists() else None' | python /edx/app/${app_repo}/manage.py shell\""
+
+    # Create Nginx config
+    echo "site_config=/edx/app/nginx/sites-available/${app_service_name}"
+    echo "cat << 'EOF' > \$site_config
+    server {
+       server_name ~^((stage|prod)-)?${app_hostname}.*;
+       listen 80;
+       rewrite ^ https://\$host\$request_uri? permanent;
+     }
+     server {
+       server_name ~^((stage|prod)-)?${app_hostname}.*;
+       listen 443 ssl;
+       ssl_certificate /etc/ssl/certs/wildcard.sandbox.edx.org.pem;
+       ssl_certificate_key /etc/ssl/private/wildcard.sandbox.edx.org.key;
+
+       location / {
+         try_files \$uri @proxy_to_app;
+       }
+       location ~ ^/(api)/ {
+          try_files \$uri @proxy_to_app;
+       }
+       location @proxy_to_app {
+          proxy_set_header X-Forwarded-Proto \$scheme;
+          proxy_set_header X-Forwarded-Port \$server_port;
+          proxy_set_header X-Forwarded-For \$remote_addr;
+          proxy_set_header Host \$http_host;
+          proxy_redirect off;
+          proxy_pass http://127.0.0.1:${app_gunicorn_port};
+       }
+       location ~ ^/static/(?P<file>.*) {
+         root /edx/var/${app_service_name};
+         try_files /staticfiles/\$file =404;
+       }
+     }
+EOF"
+     echo "ln -s  /edx/app/nginx/sites-available/${app_service_name} /etc/nginx/sites-enabled/${app_service_name}"
+     echo "service nginx reload"
+}
+
+if [[ $edx_exams == 'true' ]]; then
+    
+    app_hostname="edx-exams"
+    app_service_name="edx_exams"
+    app_repo="edx-exams"
+    app_version=$edx_exams_version
+    app_gunicorn_port=18740
+    app_cfg=EDX_EXAMS_CFG
+
+    provision_script="/var/tmp/provision-script-$$.sh"
+cat << EOF > $provision_script
+$(provision_containerized_app)
+EOF
+  
+    # copy app config file and run script to deploy app
+    ansible -c ssh -i "${deploy_host}," $deploy_host -m copy -a "src=${WORKSPACE}/configuration-internal/k8s-sandbox-config/${app_service_name}.yml dest=/var/tmp/${app_service_name}.yml" -u ubuntu -b
+    ansible -c ssh -i "${deploy_host}," $deploy_host -m script -a "${provision_script}" -u ubuntu -b
+
+    rm -f "${provision_script}"
+fi
+
 rm -f "$extra_vars_file"
 rm -f ${extra_vars_file}_clean
