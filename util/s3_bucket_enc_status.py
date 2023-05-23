62ef634666fcb8dea14ef540f31e2fbbb187fbb8
Joseph Mulloy <jmulloy@edx.org>
2023-05-23 12:55:32 -0400

    WIP: S3 bucket encryption scripts

--- util/s3_bucket_enc_status.py
+++ util/s3_bucket_enc_status.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 import boto3
 from botocore.exceptions import ClientError
 import backoff
@@ -32,35 +33,60 @@ class S3BotoWrapper:
 @click.command()
 @click.argument('input_tsv_file', required=True, type=click.File('r'))
 @click.argument('output_tsv_file', required=True, type=click.File('w'))
-def controller(input_tsv_file, output_tsv_file):
+@click.option('--filter-out-encrypted/--no-filter-out-encrypted', default=False, help="If enabled output file will not contain object keys that are already encrypted")
+def controller(input_tsv_file, output_tsv_file, filter_out_encrypted):
     client = S3BotoWrapper()
     reader = csv.reader(input_tsv_file, dialect='excel-tab')
     writer = csv.writer(output_tsv_file, dialect='excel-tab')
 
     session_valid = True
 
+    rows_read = 0
+    rows_written = 0
+    rows_not_written = 0
+    rows_skipped = 0
+    status_count = defaultdict(int)
+
     for bucket_name, object_key, encryption_status in reader:
+        rows_read += 1
+        # SSE = Server Side Encryption
+        object_sse = encryption_status
+
         # Unknown means we haven't checked yet, None means it's unencrypted
-        if encryption_status in ['None', 'Unknown']:
-            # SSE = Server Side Encryption
-            object_sse = encryption_status
+        if object_sse == 'Unknown':
             if session_valid:
                 try:
- #                   print(f"Try {object_key}")
                     object = client.get_object(bucket_name, object_key)
-                    if object.server_side_encryption is None:
-                        object_sse = 'None'
+                    object_sse = str(object.server_side_encryption)
                 except Exception as E:
-                    print(E)
-                    print(f"Except {object_key}")
+                    print(f"On object key {object_key}, caught exception {E}")
                     session_valid = False
                     pass
                 
-#            print(f"bucket {bucket_name}, object_key {object_key}, encryption_status {object_sse}")
-            if object_sse in ['None', 'Unknown']:
-                writer.writerow([bucket_name, object_key, object_sse])
         else:
-            print(f"Skipping {object_key}")
+            rows_skipped += 1
+
+        if not filter_out_encrypted or object_sse in ['None', 'Unknown']:
+            writer.writerow([bucket_name, object_key, object_sse])
+            rows_written += 1
+        else:
+            rows_not_written += 1
+
+        status_count[object_sse] += 1
+        if rows_read % 100 == 0:
+            sum_status_count = sum(status_count.values())
+            print(f"SUM(status_count): {sum_status_count}, Rows read: {rows_read}, written: {rows_written}, not_written: {rows_not_written}, skipped: {rows_skipped}, status_count: {dict(status_count)}")
+
+    sum_status_count = sum(status_count.values())
+
+    print(f"SUM(status_count): {sum_status_count}, Rows read: {rows_read}, written: {rows_written}, not_written: {rows_not_written}, skipped: {rows_skipped}, status_count: {dict(status_count)}")
+    print(f"Status count {dict(status_count)}")
+    if sum_status_count != rows_read:
+        print()
+        print(f"WARNING!!! Output file may not be trustworthy, Sum of statuses ({sum_status_count}) does not equal number of rows read ({rows_read})")
+    if rows_read != rows_written + rows_not_written:
+        print()
+        print(f"WARNING!!! Output file may not be trustworthy, Sum of rows_written and rows_not_written ({rows_written} + {rows_not_written} = {rows_written+rows_not_written}) does not equal number of rows read ({rows_read})")
 
 
 if __name__ == '__main__':
